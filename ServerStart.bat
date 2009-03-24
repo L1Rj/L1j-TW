e115a8be4e97ccb64e9fedaddedb39081233060a
Tsai Castiello <lifetime520@gmail.com>
2009-03-24 21:53:34 +0000

    1.確認舊有的lib/ 無法在本機上執行，更換新版lib/ 2.修正微調
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@5 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- ServerStart.bat
+++ ServerStart.bat
@@ -1,2 +1,2 @@
-@java -Xmx512m -Xincgc -cp l1jserver.jar;lib\c3p0-0.9.1.1.jar;lib\mysql-connector-java-5.1.5-bin.jar;lib\javolution.jar l1j.server.Server
+@java -Xmx512m -Xincgc -cp l1jserver.jar;lib\c3p0-0.9.1.2.jar;lib\mysql-connector-java-5.1.7-bin.jar;lib\javolution.jar l1j.server.Server
 @pause
