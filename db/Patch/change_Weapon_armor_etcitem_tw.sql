081c6b64419050e6b8654fee9850b15bd67bde7f
waja <waja>
2009-04-18 17:30:43 +0000

    更新部份 武器防具道具中文翻譯
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@110 96aee97a-5c11-ed42-8518-d7f33e96f5f9

---
+++ db/Patch/change_Weapon_armor_etcitem_tw.sql
@@ -0,0 +1,66 @@
+/* 20090419 waja 修正新武器名稱 */
+Update weapon Set name = '底比斯歐西里斯雙刀' Where item_id = '265';
+Update weapon Set name = '底比斯歐西里斯雙手劍' Where item_id = '266';
+Update weapon Set name = '底比斯歐西里斯弓' Where item_id = '267';
+Update weapon Set name = '底比斯歐西里斯魔杖' Where item_id = '268';
+Update weapon Set name = '幻術士魔杖' Where item_id = '269';
+Update weapon Set name = '藍寶石奇古獸' Where item_id = '270';
+Update weapon Set name = '黑曜石奇古獸' Where item_id = '271';
+Update weapon Set name = '消滅者鎖鏈劍' Where item_id = '272';
+Update weapon Set name = '破滅者鎖鏈劍' Where item_id = '273';
+
+/* 20090419 waja 修正新防具名稱 */
+
+Update armor Set name = '底比斯赫洛斯戒指' Where item_id = '21093';
+Update armor Set name = '底比斯阿比努斯戒指' Where item_id = '21094';
+Update armor Set name = '底比斯歐西里斯腰帶' Where item_id = '21095';
+Update armor Set name = '命運耳環' Where item_id = '21096';
+Update armor Set name = '射擊手耳環' Where item_id = '21097';
+Update armor Set name = '鬥劍士耳環' Where item_id = '21098';
+Update armor Set name = '大法師耳環' Where item_id = '21099';
+Update armor Set name = '幻術士斗篷' Where item_id = '21100';
+Update armor Set name = '幻術士法書' Where item_id = '21101';
+Update armor Set name = '龍騎士斗篷' Where item_id = '21102';
+Update armor Set name = '龍鱗臂甲' Where item_id = '21103';
+Update armor Set name = '水晶臂甲' Where item_id = '21104';
+Update armor Set name = '古代神射臂甲' Where item_id = '21105';
+Update armor Set name = '古代鬥士臂甲' Where item_id = '21106';
+
+/* 20090419 waja 修正新道具名稱 未完 */
+Update etcitem Set name = '路西爾斯邪念碎片' Where item_id = '49231';
+Update etcitem Set name = '初級秘笈書' Where item_id = '49232';
+Update etcitem Set name = '狩獵秘笈' Where item_id = '49233';
+Update etcitem Set name = '便當袋' Where item_id = '49234';
+Update etcitem Set name = '克特的秘密' Where item_id = '49235';
+Update etcitem Set name = '庫卡斯的證明' Where item_id = '49236';
+Update etcitem Set name = '史塔利的證明' Where item_id = '49237';
+Update etcitem Set name = '狩獵的證明' Where item_id = '49238';
+Update etcitem Set name = '消滅之意志' Where item_id = '49239';
+Update etcitem Set name = '監視者之眼' Where item_id = '49240';
+Update etcitem Set name = '祭壇的碎片' Where item_id = '49241';
+Update etcitem Set name = '底比斯歐西里斯祭壇鑰匙' Where item_id = '49242';
+Update etcitem Set name = '香菜' Where item_id = '49243';
+Update etcitem Set name = '烤奎斯坦修的螯' Where item_id = '49244';
+Update etcitem Set name = '烤格利芬肉' Where item_id = '49245';
+Update etcitem Set name = '亞力安的尾巴肉排' Where item_id = '49246';
+Update etcitem Set name = '烤巨王龜肉' Where item_id = '49247';
+Update etcitem Set name = '幼龍翅膀串燒' Where item_id = '49248';
+Update etcitem Set name = '烤飛龍肉' Where item_id = '49249';
+Update etcitem Set name = '燉深海魚肉' Where item_id = '49250';
+Update etcitem Set name = '邪惡蜥蜴蛋湯' Where item_id = '49251';
+Update etcitem Set name = '特別的烤奎斯坦修的螯' Where item_id = '49252';
+Update etcitem Set name = '特別的烤格利芬肉' Where item_id = '49253';
+Update etcitem Set name = '特別的亞力安的尾巴肉排' Where item_id = '49254';
+Update etcitem Set name = '特別的烤巨王龜肉' Where item_id = '49255';
+Update etcitem Set name = '特別的幼龍翅膀串燒' Where item_id = '49256';
+Update etcitem Set name = '特別的烤飛龍肉' Where item_id = '49257';
+Update etcitem Set name = '特別的燉深海魚肉' Where item_id = '49258';
+Update etcitem Set name = '特別的邪惡蜥蜴蛋湯' Where item_id = '49259';
+Update etcitem Set name = '奎斯坦修的螯' Where item_id = '49260';
+Update etcitem Set name = '格利芬肉' Where item_id = '49261';
+Update etcitem Set name = '亞力安的尾巴' Where item_id = '49262';
+Update etcitem Set name = '巨王龜肉' Where item_id = '49263';
+Update etcitem Set name = '幼龍翅膀' Where item_id = '49264';
+Update etcitem Set name = '飛龍肉' Where item_id = '49265';
+Update etcitem Set name = '深海魚肉' Where item_id = '49266';
+Update etcitem Set name = '邪惡蜥蜴蛋' Where item_id = '49267';
