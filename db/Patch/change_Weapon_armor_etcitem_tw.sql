27e52df4898731e0b16ff8661ed10b0aa731bd0a
Tsai Castiello <lifetime520@gmail.com>
2009-04-23 04:13:59 +0000

    劃分 ：繁化/修正 兩大格式
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@138 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- db/Patch/change_Weapon_armor_etcitem_tw.sql
+++
@@ -1,2 +0,0 @@
-/* 20090419 waja 修正新道具名稱 未完 */
-/* 20090422 lifetime520 Update To DB-SVN */
--- db/Patch/update_rev1732_characters.sql
+++
@@ -1,7 +0,0 @@
-/* 20090418 l1j rev 1736 角色欄位變更 */
-alter table characters add  `OriginalStr` int(3) default '0';
-alter table characters add  `OriginalCon` int(3) default '0';
-alter table characters add  `OriginalDex` int(3) default '0';
-alter table characters add  `OriginalCha` int(3) default '0';
-alter table characters add  `OriginalInt` int(3) default '0';
-alter table characters add  `OriginalWis` int(3) default '0';
