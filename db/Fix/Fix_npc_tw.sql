aa061ce0baf25b42b20a0bd4d99d383cc0ba6b55
waja <waja>
2009-04-25 18:31:23 +0000

    修正妖森守護神行走速度
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@153 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- db/Fix/Fix_npc_tw.sql
+++ db/Fix/Fix_npc_tw.sql
@@ -2,5 +2,9 @@
 Update npc Set lawful = '65535' Where npcid = '70503';
 Update npc Set lawful = '-65535' Where npcid = '70511';
 /* 守護神行走速度降低 */
-Update npc Set passispeed = '120' Where npcid = '70848';
-Update npc Set passispeed = '120' Where npcid = '70850';
+<<<<<<< .mine
+Update npc Set passispeed = '1700' Where npcid = '70848'; /* 安特 */
+Update npc Set passispeed = '1500' Where npcid = '70850'; /* 潘 */
+Update npc Set passispeed = '1500' Where npcid = '70846'; /* 芮克妮 */
+Update npc Set passispeed = '1200' Where npcid = '70851'; /* 精靈 */
+
