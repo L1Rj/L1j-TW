583255baf4ce79f2732a86647750c9c7400f7ea7
waja <waja>
2009-04-25 11:02:16 +0000

    修正書板重量 守護神走速再降低 怪物新增書板道具掉落
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@148 96aee97a-5c11-ed42-8518-d7f33e96f5f9

---
+++ db/Fix/Fix_etcitem.sql
@@ -0,0 +1,16 @@
+/* 20090425 龍騎士技能書道具重量修正 資料來源天堂透視鏡 */
+Update etcitem Set weight = '7' Where item_id = '49102';
+Update etcitem Set weight = '7' Where item_id = '49103';
+Update etcitem Set weight = '7' Where item_id = '49104';
+Update etcitem Set weight = '7' Where item_id = '49105';
+Update etcitem Set weight = '7' Where item_id = '49106';
+Update etcitem Set weight = '7' Where item_id = '49107';
+Update etcitem Set weight = '7' Where item_id = '49108';
+Update etcitem Set weight = '7' Where item_id = '49109';
+Update etcitem Set weight = '7' Where item_id = '49110';
+Update etcitem Set weight = '7' Where item_id = '49111';
+Update etcitem Set weight = '7' Where item_id = '49112';
+Update etcitem Set weight = '7' Where item_id = '49113';
+Update etcitem Set weight = '7' Where item_id = '49114';
+Update etcitem Set weight = '7' Where item_id = '49115';
+Update etcitem Set weight = '7' Where item_id = '49116';
--- db/Fix/Fix_npc_tw.sql
+++ db/Fix/Fix_npc_tw.sql
@@ -2,5 +2,5 @@
 Update npc Set lawful = '65535' Where npcid = '70503';
 Update npc Set lawful = '-65535' Where npcid = '70511';
 /* 守護神行走速度降低 */
-Update npc Set passispeed = '240' Where npcid = '70848';
-Update npc Set passispeed = '240' Where npcid = '70850';
+Update npc Set passispeed = '120' Where npcid = '70848';
+Update npc Set passispeed = '120' Where npcid = '70850';
