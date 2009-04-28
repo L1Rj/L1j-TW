67d702147945fb4b03602637b0b09e396512ec2a
waja <waja>
2009-04-28 18:04:41 +0000

    更新奇岩武器防具販賣(祝福&詛咒)
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@177 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- db/Fix/Fix_shop_tw.sql
+++ db/Fix/Fix_shop_tw.sql
@@ -1,25 +1,25 @@
 /* l1j db 20090416 修正奇岩范吉爾(防具商人 npcid 70032)收購物品 */
-Update shop Set purchasing_price = '1500' Where npc_id='70032' and item_id='20011';
-Update shop Set purchasing_price = '11000' Where npc_id='70032' and item_id='20013';
-Update shop Set purchasing_price = '7500' Where npc_id='70032' and item_id='20014';
-Update shop Set purchasing_price = '9000' Where npc_id='70032' and item_id='20015';
-Update shop Set purchasing_price = '6000' Where npc_id='70032' and item_id='20036';
-Update shop Set purchasing_price = '100' Where npc_id='70032' and item_id='20043';
-Update shop Set purchasing_price = '50' Where npc_id='70032' and item_id='20089';
-Update shop Set purchasing_price = '250' Where npc_id='70032' and item_id='20096';
-Update shop Set purchasing_price = '11500' Where npc_id='70032' and item_id='20101';
-Update shop Set purchasing_price = '4000' Where npc_id='70032' and item_id='20110';
-Update shop Set purchasing_price = '500' Where npc_id='70032' and item_id='20114';
-Update shop Set purchasing_price = '10000' Where npc_id='70032' and item_id='20115';
-Update shop Set purchasing_price = '1000' Where npc_id='70032' and item_id='20122';
-Update shop Set purchasing_price = '3000' Where npc_id='70032' and item_id='20125';
-Update shop Set purchasing_price = '150' Where npc_id='70032' and item_id='20147';
-Update shop Set purchasing_price = '8000' Where npc_id='70032' and item_id='20149';
-Update shop Set purchasing_price = '18500' Where npc_id='70032' and item_id='20154';
-Update shop Set purchasing_price = '10000' Where npc_id='70032' and item_id='20229';
-Update shop Set purchasing_price = '3500' Where npc_id='70032' and item_id='20231';
-Update shop Set purchasing_price = '30' Where npc_id='70032' and item_id='20239';
-Update shop Set purchasing_price = '600' Where npc_id='70032' and item_id='20242';
+Update shop Set purchasing_price = '1500' Where npc_id='70032' and item_id='20011';/* 抗魔法頭盔 */
+Update shop Set purchasing_price = '11000' Where npc_id='70032' and item_id='20013';/* 敏捷魔法頭盔 */
+Update shop Set purchasing_price = '7500' Where npc_id='70032' and item_id='20014'; /* 治癒魔法頭盔 */
+Update shop Set purchasing_price = '9000' Where npc_id='70032' and item_id='20015';/* 力量魔法頭盔 */
+Update shop Set purchasing_price = '6000' Where npc_id='70032' and item_id='20036';/* 夜視頭盔 */
+Update shop Set purchasing_price = '100' Where npc_id='70032' and item_id='20043';/* 鋼盔 */
+Update shop Set purchasing_price = '50' Where npc_id='70032' and item_id='20089'; /*  小藤甲 */
+Update shop Set purchasing_price = '250' Where npc_id='70032' and item_id='20096'; /* 環甲 */
+Update shop Set purchasing_price = '11500' Where npc_id='70032' and item_id='20101'; /* 皮甲 */
+Update shop Set purchasing_price = '4000' Where npc_id='70032' and item_id='20110';/* 抗魔法鏈甲 */
+Update shop Set purchasing_price = '500' Where npc_id='70032' and item_id='20114'; /* 綿質長袍 */
+Update shop Set purchasing_price = '10000' Where npc_id='70032' and item_id='20115'; /* 藤甲 */
+Update shop Set purchasing_price = '1000' Where npc_id='70032' and item_id='20122';/* 鱗甲 */
+Update shop Set purchasing_price = '3000' Where npc_id='70032' and item_id='20125'; /* 鏈甲 */
+Update shop Set purchasing_price = '150' Where npc_id='70032' and item_id='20147'; /* 銀釘皮甲 */
+Update shop Set purchasing_price = '8000' Where npc_id='70032' and item_id='20149';/* 青銅盔甲 */
+Update shop Set purchasing_price = '18500' Where npc_id='70032' and item_id='20154';/* 金屬盔甲 */
+Update shop Set purchasing_price = '10000' Where npc_id='70032' and item_id='20229';/* 反射之盾 */
+Update shop Set purchasing_price = '3500' Where npc_id='70032' and item_id='20231';/* 塔盾 */
+Update shop Set purchasing_price = '30' Where npc_id='70032' and item_id='20239';/* 小盾牌 */
+Update shop Set purchasing_price = '600' Where npc_id='70032' and item_id='20242';/* 大盾牌 */
 
 /* l1j db 20090416 修正奇岩迪歐(寶石商人 npcid 70027)收購物品 */
 Update shop Set purchasing_price = '500' Where npc_id='70027' and item_id='40044';
@@ -36,36 +36,72 @@ Update shop Set purchasing_price = '5000' Where npc_id='70027' and item_id='4005
 Update shop Set purchasing_price = '5000' Where npc_id='70027' and item_id='40055';
 
 /* l1j db 20090416 修正奇岩溫諾(武器商人 npcid 70039)收購物品 */
-Update shop Set purchasing_price = '15' Where npc_id='70039' and item_id='23';
-Update shop Set purchasing_price = '50' Where npc_id='70039' and item_id='24';
-Update shop Set purchasing_price = '900' Where npc_id='70039' and item_id='25';
-Update shop Set purchasing_price = '500' Where npc_id='70039' and item_id='27';
-Update shop Set purchasing_price = '6500' Where npc_id='70039' and item_id='29';
-Update shop Set purchasing_price = '1500' Where npc_id='70039' and item_id='31';
-Update shop Set purchasing_price = '1250' Where npc_id='70039' and item_id='32';
-Update shop Set purchasing_price = '2500' Where npc_id='70039' and item_id='37';
-Update shop Set purchasing_price = '10000' Where npc_id='70039' and item_id='41';
-Update shop Set purchasing_price = '9000' Where npc_id='70039' and item_id='52';
-Update shop Set purchasing_price = '20' Where npc_id='70039' and item_id='90';
-Update shop Set purchasing_price = '90' Where npc_id='70039' and item_id='92';
-Update shop Set purchasing_price = '22' Where npc_id='70039' and item_id='93';
-Update shop Set purchasing_price = '250' Where npc_id='70039' and item_id='94';
-Update shop Set purchasing_price = '30' Where npc_id='70039' and item_id='95';
-Update shop Set purchasing_price = '100' Where npc_id='70039' and item_id='97';
-Update shop Set purchasing_price = '180' Where npc_id='70039' and item_id='98';
-Update shop Set purchasing_price = '5000' Where npc_id='70039' and item_id='102';
-Update shop Set purchasing_price = '1000' Where npc_id='70039' and item_id='103';
-Update shop Set purchasing_price = '4000' Where npc_id='70039' and item_id='104';
-Update shop Set purchasing_price = '8000' Where npc_id='70039' and item_id='106';
-Update shop Set purchasing_price = '4500' Where npc_id='70039' and item_id='125';
-Update shop Set purchasing_price = '150' Where npc_id='70039' and item_id='128';
-Update shop Set purchasing_price = '1250' Where npc_id='70039' and item_id='129';
-Update shop Set purchasing_price = '10' Where npc_id='70039' and item_id='136';
-Update shop Set purchasing_price = '15' Where npc_id='70039' and item_id='138';
-Update shop Set purchasing_price = '20' Where npc_id='70039' and item_id='139';
-Update shop Set purchasing_price = '500' Where npc_id='70039' and item_id='141';
-Update shop Set purchasing_price = '4000' Where npc_id='70039' and item_id='142';
-Update shop Set purchasing_price = '350' Where npc_id='70039' and item_id='143';
-Update shop Set purchasing_price = '1750' Where npc_id='70039' and item_id='146';
-Update shop Set purchasing_price = '6000' Where npc_id='70039' and item_id='148';
-Update shop Set purchasing_price = '25' Where npc_id='70039' and item_id='172';
+Update shop Set purchasing_price = '15' Where npc_id='70039' and item_id='23';/* 闊劍 */
+Update shop Set purchasing_price = '50' Where npc_id='70039' and item_id='24';/* 短劍 */
+Update shop Set purchasing_price = '900' Where npc_id='70039' and item_id='25';/* 銀劍 */
+Update shop Set purchasing_price = '900' Where npc_id='70039' and item_id='100025';/* 銀劍 */
+Update shop Set purchasing_price = '500' Where npc_id='70039' and item_id='27';/* 彎刀 */
+Update shop Set purchasing_price = '6500' Where npc_id='70039' and item_id='29';/* 銀長劍 */
+Update shop Set purchasing_price = '1500' Where npc_id='70039' and item_id='31';/* 長劍 */
+Update shop Set purchasing_price = '1250' Where npc_id='70039' and item_id='32';/* 侵略者之劍 */
+Update shop Set purchasing_price = '2500' Where npc_id='70039' and item_id='37';/* 大馬士革刀 */
+Update shop Set purchasing_price = '10000' Where npc_id='70039' and item_id='41';/* 武士刀 */
+Update shop Set purchasing_price = '9000' Where npc_id='70039' and item_id='52';/* 雙手劍 */
+Update shop Set purchasing_price = '20' Where npc_id='70039' and item_id='90';/* 巴迪須 */
+Update shop Set purchasing_price = '90' Where npc_id='70039' and item_id='92';/* 柴刀 */
+Update shop Set purchasing_price = '22' Where npc_id='70039' and item_id='93';/* 三叉戟 */
+Update shop Set purchasing_price = '250' Where npc_id='70039' and item_id='94';/* 帕提森 */
+Update shop Set purchasing_price = '30' Where npc_id='70039' and item_id='95';/* 矛 */
+Update shop Set purchasing_price = '100' Where npc_id='70039' and item_id='97';/* 吉薩 */
+Update shop Set purchasing_price = '180' Where npc_id='70039' and item_id='98';/* 闊矛 */
+Update shop Set purchasing_price = '5000' Where npc_id='70039' and item_id='102';/* 露西錘 */
+Update shop Set purchasing_price = '1000' Where npc_id='70039' and item_id='103';/* 戟 */
+Update shop Set purchasing_price = '4000' Where npc_id='70039' and item_id='104';/* 法丘 */
+Update shop Set purchasing_price = '8000' Where npc_id='70039' and item_id='106';/* 貝卡合金 */
+Update shop Set purchasing_price = '4500' Where npc_id='70039' and item_id='125';/* 巫術魔法杖 */
+Update shop Set purchasing_price = '150' Where npc_id='70039' and item_id='128';/* 橡木魔法杖 */
+Update shop Set purchasing_price = '1250' Where npc_id='70039' and item_id='129';/* 美基魔法杖 */
+Update shop Set purchasing_price = '10' Where npc_id='70039' and item_id='136';/* 斧 */
+Update shop Set purchasing_price = '15' Where npc_id='70039' and item_id='138';/* 木棒 */
+Update shop Set purchasing_price = '20' Where npc_id='70039' and item_id='139';/* 弗萊爾 */
+Update shop Set purchasing_price = '500' Where npc_id='70039' and item_id='141';/* 戰錘 */
+Update shop Set purchasing_price = '4000' Where npc_id='70039' and item_id='142';/* 銀斧 */
+Update shop Set purchasing_price = '350' Where npc_id='70039' and item_id='143';/* 戰斧 */
+Update shop Set purchasing_price = '1750' Where npc_id='70039' and item_id='146';/* 流星錘 */
+Update shop Set purchasing_price = '6000' Where npc_id='70039' and item_id='148';/* 巨斧 */
+Update shop Set purchasing_price = '25' Where npc_id='70039' and item_id='172';/* 弓 */
+
+/* 20090429 修正奇岩 祝福 詛咒裝備出售 */
+INSERT INTO `shop` VALUES
+/* 防具商人 */
+(70032, 120011, 21, -1, 0, 1500),/* 祝福抗魔法頭盔 */
+(70032, 120101, 22, -1, 0, 11500),/* 祝福皮甲 */
+(70032, 220101, 23, -1, 0, 11500),/* 詛咒皮甲 */
+(70032, 120043, 24, -1, 0, 100),/* 祝福鋼盔 */
+(70032, 220043, 25, -1, 0, 100),/* 詛咒鋼盔 */
+(70032, 220115, 26, -1, 0, 10000),/* 詛咒藤甲 */
+(70032, 220122, 27, -1, 0, 1000),/* 詛咒鱗甲 */
+(70032, 220125, 28, -1, 0, 3000),/* 詛咒鏈甲 */
+(70032, 220147, 29, -1, 0, 150),/* 詛咒銀釘皮甲 */
+(70032, 120149, 30, -1, 0, 8000),/* 祝福青銅盔甲 */
+(70032, 120154, 31, -1, 0, 18500),/* 祝福金屬盔甲 */
+(70032, 220149, 32, -1, 0, 18500),/* 詛咒金屬盔甲 */
+(70032, 220242, 33, -1, 0, 600),/* 祝福大盾牌 */
+/* 武器商人 */
+(70039, 1000027, 35, -1, 0, 500),/* 祝福彎刀 */
+(70039, 2000027, 36, -1, 0, 500),/* 詛咒彎刀 */
+(70039, 1000029, 37, -1, 0, 6500),/* 祝福銀長劍 */
+(70039, 2000032, 38, -1, 0, 1250),/* 詛咒侵略者之劍 */
+(70039, 1000037, 39, -1, 0, 2500),/* 祝福大馬士革刀 */
+(70039, 1000041, 40, -1, 0, 10000),/* 祝福武士刀 */
+(70039, 2000041, 41, -1, 0, 10000),/* 詛咒武士刀 */
+(70039, 1000052, 42, -1, 0, 9000),/* 祝福雙手劍 */
+(70039, 2000052, 43, -1, 0, 9000),/* 詛咒雙手劍 */
+(70039, 100095, 44, -1, 0, 30),/* 祝福矛 */
+(70039, 100098, 45, -1, 0, 180),/* 祝福闊矛 */
+(70039, 100102, 46, -1, 0, 5000),/* 祝福露西錘 */
+(70039, 100103, 47, -1, 0, 1000),/* 祝福戟 */
+/* l1j db 20090429 修正 奇岩愛弗特 70038 收購抗魔法斗篷 */
+(70038, 20056, 3, -1, 0, 100),/* 抗魔法斗篷 */
+(70038, 120056, 4, -1, 0, 100),/* 祝福抗魔法斗篷 */
+(70038, 220056, 5, -1, 0, 100);/* 詛咒抗魔法斗篷 */
