57a84da596168fda6a74e622872281d9b9cc820b
waja <waja>
2009-04-28 14:37:50 +0000

    l1j 1772
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@173 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- src/l1j/server/server/clientpackets/C_ItemUSe.java
+++ src/l1j/server/server/clientpackets/C_ItemUSe.java
@@ -847,9 +847,16 @@ public class C_ItemUSe extends ClientBasePacket {
 						|| itemId == 41157) { // 憎惡の鱗
 					usePolyScale(pc, itemId);
 					pc.getInventory().removeItem(l1iteminstance, 1);
-				} else if (itemId == 41143 // ラバーボーンヘッド變身ポーション
-						|| itemId == 41144 // ラバーボーンアーチャー變身ポーション
-						|| itemId == 41145) { // ラバーボーンナイフ變身ポーション
+				} else if (itemId == 41143 // ラバーボーンヘッド変身ポーション
+						|| itemId == 41144 // ラバーボーンアーチャー変身ポーション
+						|| itemId == 41145 // ラバーボーンナイフ変身ポーション
+						|| itemId == 49149 // シャルナの変身スクロール（レベル30）
+						|| itemId == 49150 // シャルナの変身スクロール（レベル40）
+						|| itemId == 49151 // シャルナの変身スクロール（レベル52）
+						|| itemId == 49152 // シャルナの変身スクロール（レベル55）
+						|| itemId == 49153 // シャルナの変身スクロール（レベル60）
+						|| itemId == 49154 // シャルナの変身スクロール（レベル65）
+						|| itemId == 49155) { // シャルナの変身スクロール（レベル70）
 					usePolyPotion(pc, itemId);
 					pc.getInventory().removeItem(l1iteminstance, 1);
 				} else if (itemId == 40317) { // 砥石
@@ -3435,12 +3442,208 @@ public class C_ItemUSe extends ClientBasePacket {
 
 	private void usePolyPotion(L1PcInstance pc, int itemId) {
 		int polyId = 0;
-		if (itemId == 41143) {
+		if (itemId == 41143) { // ラバーボーンヘッド変身ポーション
 			polyId = 6086;
-		} else if (itemId == 41144) {
+		} else if (itemId == 41144) { // ラバーボーンアーチャー変身ポーション
 			polyId = 6087;
-		} else if (itemId == 41145) {
+		} else if (itemId == 41145) { // ラバーボーンナイフ変身ポーション
 			polyId = 6088;
+		} else if (itemId == 49149 && pc.get_sex() == 0 && pc.isCrown()) { // シャルナの変身スクロール（レベル30）
+			polyId = 6822;
+		} else if (itemId == 49149 && pc.get_sex() == 1 && pc.isCrown()) {
+			polyId = 6823;
+		} else if (itemId == 49149 && pc.get_sex() == 0 && pc.isKnight()) {
+			polyId = 6824;
+		} else if (itemId == 49149 && pc.get_sex() == 1 && pc.isKnight()) {
+			polyId = 6825;
+		} else if (itemId == 49149 && pc.get_sex() == 0 && pc.isElf()) {
+			polyId = 6826;
+		} else if (itemId == 49149 && pc.get_sex() == 1 && pc.isElf()) {
+			polyId = 6827;
+		} else if (itemId == 49149 && pc.get_sex() == 0 && pc.isWizard()) {
+			polyId = 6828;
+		} else if (itemId == 49149 && pc.get_sex() == 1 && pc.isWizard()) {
+			polyId = 6829;
+		} else if (itemId == 49149 && pc.get_sex() == 0 && pc.isDarkelf()) {
+			polyId = 6830;
+		} else if (itemId == 49149 && pc.get_sex() == 1 && pc.isDarkelf()) {
+			polyId = 6831;
+		} else if (itemId == 49149 && pc.get_sex() == 0 && pc.isDragonKnight()) {
+			polyId = 7139;
+		} else if (itemId == 49149 && pc.get_sex() == 1 && pc.isDragonKnight()) {
+			polyId = 7140;
+		} else if (itemId == 49149 && pc.get_sex() == 0 && pc.isIllusionist()) {
+			polyId = 7141;
+		} else if (itemId == 49149 && pc.get_sex() == 1 && pc.isIllusionist()) {
+			polyId = 7142;
+		} else if (itemId == 49150 && pc.get_sex() == 0 && pc.isCrown()) { // シャルナの変身スクロール（レベル40）
+			polyId = 6832;
+		} else if (itemId == 49150 && pc.get_sex() == 1 && pc.isCrown()) {
+			polyId = 6833;
+		} else if (itemId == 49150 && pc.get_sex() == 0 && pc.isKnight()) {
+			polyId = 6834;
+		} else if (itemId == 49150 && pc.get_sex() == 1 && pc.isKnight()) {
+			polyId = 6835;
+		} else if (itemId == 49150 && pc.get_sex() == 0 && pc.isElf()) {
+			polyId = 6836;
+		} else if (itemId == 49150 && pc.get_sex() == 1 && pc.isElf()) {
+			polyId = 6837;
+		} else if (itemId == 49150 && pc.get_sex() == 0 && pc.isWizard()) {
+			polyId = 6838;
+		} else if (itemId == 49150 && pc.get_sex() == 1 && pc.isWizard()) {
+			polyId = 6839;
+		} else if (itemId == 49150 && pc.get_sex() == 0 && pc.isDarkelf()) {
+			polyId = 6840;
+		} else if (itemId == 49150 && pc.get_sex() == 1 && pc.isDarkelf()) {
+			polyId = 6841;
+		} else if (itemId == 49150 && pc.get_sex() == 0 && pc.isDragonKnight()) {
+			polyId = 7143;
+		} else if (itemId == 49150 && pc.get_sex() == 1 && pc.isDragonKnight()) {
+			polyId = 7144;
+		} else if (itemId == 49150 && pc.get_sex() == 0 && pc.isIllusionist()) {
+			polyId = 7145;
+		} else if (itemId == 49150 && pc.get_sex() == 1 && pc.isIllusionist()) {
+			polyId = 7146;
+		} else if (itemId == 49151 && pc.get_sex() == 0 && pc.isCrown()) { // シャルナの変身スクロール（レベル52）
+			polyId = 6842;
+		} else if (itemId == 49151 && pc.get_sex() == 1 && pc.isCrown()) {
+			polyId = 6843;
+		} else if (itemId == 49151 && pc.get_sex() == 0 && pc.isKnight()) {
+			polyId = 6844;
+		} else if (itemId == 49151 && pc.get_sex() == 1 && pc.isKnight()) {
+			polyId = 6845;
+		} else if (itemId == 49151 && pc.get_sex() == 0 && pc.isElf()) {
+			polyId = 6846;
+		} else if (itemId == 49151 && pc.get_sex() == 1 && pc.isElf()) {
+			polyId = 6847;
+		} else if (itemId == 49151 && pc.get_sex() == 0 && pc.isWizard()) {
+			polyId = 6848;
+		} else if (itemId == 49151 && pc.get_sex() == 1 && pc.isWizard()) {
+			polyId = 6849;
+		} else if (itemId == 49151 && pc.get_sex() == 0 && pc.isDarkelf()) {
+			polyId = 6850;
+		} else if (itemId == 49151 && pc.get_sex() == 1 && pc.isDarkelf()) {
+			polyId = 6851;
+		} else if (itemId == 49151 && pc.get_sex() == 0 && pc.isDragonKnight()) {
+			polyId = 7147;
+		} else if (itemId == 49151 && pc.get_sex() == 1 && pc.isDragonKnight()) {
+			polyId = 7148;
+		} else if (itemId == 49151 && pc.get_sex() == 0 && pc.isIllusionist()) {
+			polyId = 7149;
+		} else if (itemId == 49151 && pc.get_sex() == 1 && pc.isIllusionist()) {
+			polyId = 7150;
+		} else if (itemId == 49152 && pc.get_sex() == 0 && pc.isCrown()) { // シャルナの変身スクロール（レベル55）
+			polyId = 6852;
+		} else if (itemId == 49152 && pc.get_sex() == 1 && pc.isCrown()) {
+			polyId = 6853;
+		} else if (itemId == 49152 && pc.get_sex() == 0 && pc.isKnight()) {
+			polyId = 6854;
+		} else if (itemId == 49152 && pc.get_sex() == 1 && pc.isKnight()) {
+			polyId = 6855;
+		} else if (itemId == 49152 && pc.get_sex() == 0 && pc.isElf()) {
+			polyId = 6856;
+		} else if (itemId == 49152 && pc.get_sex() == 1 && pc.isElf()) {
+			polyId = 6857;
+		} else if (itemId == 49152 && pc.get_sex() == 0 && pc.isWizard()) {
+			polyId = 6858;
+		} else if (itemId == 49152 && pc.get_sex() == 1 && pc.isWizard()) {
+			polyId = 6859;
+		} else if (itemId == 49152 && pc.get_sex() == 0 && pc.isDarkelf()) {
+			polyId = 6860;
+		} else if (itemId == 49152 && pc.get_sex() == 1 && pc.isDarkelf()) {
+			polyId = 6861;
+		} else if (itemId == 49152 && pc.get_sex() == 0 && pc.isDragonKnight()) {
+			polyId = 7151;
+		} else if (itemId == 49152 && pc.get_sex() == 1 && pc.isDragonKnight()) {
+			polyId = 7152;
+		} else if (itemId == 49152 && pc.get_sex() == 0 && pc.isIllusionist()) {
+			polyId = 7153;
+		} else if (itemId == 49152 && pc.get_sex() == 1 && pc.isIllusionist()) {
+			polyId = 7154;
+		} else if (itemId == 49153 && pc.get_sex() == 0 && pc.isCrown()) { // シャルナの変身スクロール（レベル60）
+			polyId = 6862;
+		} else if (itemId == 49153 && pc.get_sex() == 1 && pc.isCrown()) {
+			polyId = 6863;
+		} else if (itemId == 49153 && pc.get_sex() == 0 && pc.isKnight()) {
+			polyId = 6864;
+		} else if (itemId == 49153 && pc.get_sex() == 1 && pc.isKnight()) {
+			polyId = 6865;
+		} else if (itemId == 49153 && pc.get_sex() == 0 && pc.isElf()) {
+			polyId = 6866;
+		} else if (itemId == 49153 && pc.get_sex() == 1 && pc.isElf()) {
+			polyId = 6867;
+		} else if (itemId == 49153 && pc.get_sex() == 0 && pc.isWizard()) {
+			polyId = 6868;
+		} else if (itemId == 49153 && pc.get_sex() == 1 && pc.isWizard()) {
+			polyId = 6869;
+		} else if (itemId == 49153 && pc.get_sex() == 0 && pc.isDarkelf()) {
+			polyId = 6870;
+		} else if (itemId == 49153 && pc.get_sex() == 1 && pc.isDarkelf()) {
+			polyId = 6871;
+		} else if (itemId == 49153 && pc.get_sex() == 0 && pc.isDragonKnight()) {
+			polyId = 7155;
+		} else if (itemId == 49153 && pc.get_sex() == 1 && pc.isDragonKnight()) {
+			polyId = 7156;
+		} else if (itemId == 49153 && pc.get_sex() == 0 && pc.isIllusionist()) {
+			polyId = 7157;
+		} else if (itemId == 49153 && pc.get_sex() == 1 && pc.isIllusionist()) {
+			polyId = 7158;
+		} else if (itemId == 49154 && pc.get_sex() == 0 && pc.isCrown()) { // シャルナの変身スクロール（レベル65）
+			polyId = 6872;
+		} else if (itemId == 49154 && pc.get_sex() == 1 && pc.isCrown()) {
+			polyId = 6873;
+		} else if (itemId == 49154 && pc.get_sex() == 0 && pc.isKnight()) {
+			polyId = 6874;
+		} else if (itemId == 49154 && pc.get_sex() == 1 && pc.isKnight()) {
+			polyId = 6875;
+		} else if (itemId == 49154 && pc.get_sex() == 0 && pc.isElf()) {
+			polyId = 6876;
+		} else if (itemId == 49154 && pc.get_sex() == 1 && pc.isElf()) {
+			polyId = 6877;
+		} else if (itemId == 49154 && pc.get_sex() == 0 && pc.isWizard()) {
+			polyId = 6878;
+		} else if (itemId == 49154 && pc.get_sex() == 1 && pc.isWizard()) {
+			polyId = 6879;
+		} else if (itemId == 49154 && pc.get_sex() == 0 && pc.isDarkelf()) {
+			polyId = 6880;
+		} else if (itemId == 49154 && pc.get_sex() == 1 && pc.isDarkelf()) {
+			polyId = 6881;
+		} else if (itemId == 49154 && pc.get_sex() == 0 && pc.isDragonKnight()) {
+			polyId = 7159;
+		} else if (itemId == 49154 && pc.get_sex() == 1 && pc.isDragonKnight()) {
+			polyId = 7160;
+		} else if (itemId == 49154 && pc.get_sex() == 0 && pc.isIllusionist()) {
+			polyId = 7161;
+		} else if (itemId == 49154 && pc.get_sex() == 1 && pc.isIllusionist()) {
+			polyId = 7162;
+		} else if (itemId == 49155 && pc.get_sex() == 0 && pc.isCrown()) { // シャルナの変身スクロール（レベル70）
+			polyId = 6882;
+		} else if (itemId == 49155 && pc.get_sex() == 1 && pc.isCrown()) {
+			polyId = 6883;
+		} else if (itemId == 49155 && pc.get_sex() == 0 && pc.isKnight()) {
+			polyId = 6884;
+		} else if (itemId == 49155 && pc.get_sex() == 1 && pc.isKnight()) {
+			polyId = 6885;
+		} else if (itemId == 49155 && pc.get_sex() == 0 && pc.isElf()) {
+			polyId = 6886;
+		} else if (itemId == 49155 && pc.get_sex() == 1 && pc.isElf()) {
+			polyId = 6887;
+		} else if (itemId == 49155 && pc.get_sex() == 0 && pc.isWizard()) {
+			polyId = 6888;
+		} else if (itemId == 49155 && pc.get_sex() == 1 && pc.isWizard()) {
+			polyId = 6889;
+		} else if (itemId == 49155 && pc.get_sex() == 0 && pc.isDarkelf()) {
+			polyId = 6890;
+		} else if (itemId == 49155 && pc.get_sex() == 1 && pc.isDarkelf()) {
+			polyId = 6891;
+		} else if (itemId == 49155 && pc.get_sex() == 0 && pc.isDragonKnight()) {
+			polyId = 7163;
+		} else if (itemId == 49155 && pc.get_sex() == 1 && pc.isDragonKnight()) {
+			polyId = 7164;
+		} else if (itemId == 49155 && pc.get_sex() == 0 && pc.isIllusionist()) {
+			polyId = 7165;
+		} else if (itemId == 49155 && pc.get_sex() == 1 && pc.isIllusionist()) {
+			polyId = 7166;
 		}
 		L1PolyMorph.doPoly(pc, polyId, 1800, L1PolyMorph.MORPH_BY_ITEMMAGIC);
 	}
