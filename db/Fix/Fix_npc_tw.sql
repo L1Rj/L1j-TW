ec5932bbbd349c9b41c20dd713bdf57c7f828a5f
waja <waja>
2009-05-09 16:07:37 +0000

    修正弱化NPC為被動
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@249 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- db/Fix/Fix_npc_tw.sql
+++ db/Fix/Fix_npc_tw.sql
@@ -8,5 +8,36 @@ Update npc Set passispeed = '1500' Where npcid = '70850'; /* 潘 */
 Update npc Set passispeed = '1500' Where npcid = '70846'; /* 芮克妮 */
 Update npc Set passispeed = '1200' Where npcid = '70851'; /* 精靈 */
 
-/* 20090506 刪除日版專用NPC分佈 */
-delete from spawnlist_npc where npc_templateid = 71035 ;
+/* 20090510 修正弱化NPC為非主動 */
+Update npc Set agro = 0 Where npcid = '45026';
+Update npc Set agro = 0 Where npcid = '45028';
+Update npc Set agro = 0 Where npcid = '45035';
+Update npc Set agro = 0 Where npcid = '45037';
+Update npc Set agro = 0 Where npcid = '45038';
+Update npc Set agro = 0 Where npcid = '45052';
+Update npc Set agro = 0 Where npcid = '45056';
+Update npc Set agro = 0 Where npcid = '45057';
+Update npc Set agro = 0 Where npcid = '45061';
+Update npc Set agro = 0 Where npcid = '45062';
+Update npc Set agro = 0 Where npcid = '45063';
+Update npc Set agro = 0 Where npcid = '45067';
+Update npc Set agro = 0 Where npcid = '45069';
+Update npc Set agro = 0 Where npcid = '45070';
+Update npc Set agro = 0 Where npcid = '45071';
+Update npc Set agro = 0 Where npcid = '45072';
+Update npc Set agro = 0 Where npcid = '45073';
+Update npc Set agro = 0 Where npcid = '45074';
+Update npc Set agro = 0 Where npcid = '45075';
+Update npc Set agro = 0 Where npcid = '45076';
+Update npc Set agro = 0 Where npcid = '45078';
+Update npc Set agro = 0 Where npcid = '45080';
+Update npc Set agro = 0 Where npcid = '45081';
+Update npc Set agro = 0 Where npcid = '45085';
+Update npc Set agro = 0 Where npcid = '45086';
+Update npc Set agro = 0 Where npcid = '45090';
+Update npc Set agro = 0 Where npcid = '45091';
+Update npc Set agro = 0 Where npcid = '45095';
+Update npc Set agro = 0 Where npcid = '45096';
+Update npc Set agro = 0 Where npcid = '45111';
+Update npc Set agro = 0 Where npcid = '45113';
+Update npc Set agro = 0 Where npcid = '45114';
--- db/Fix/Fix_spawnlist_npc_tw.sql
+++ db/Fix/Fix_spawnlist_npc_tw.sql
@@ -1,1 +1,4 @@
 Update spawnlist_npc Set npc_templateid = '81111' Where id = '89543'; /* 亞丁守護塔 ~ 原本的打不倒 */
+
+/* 20090506 刪除日版專用NPC分佈 */
+delete from spawnlist_npc where npc_templateid = 71035 ;
