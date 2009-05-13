2749ba93509b7f714c9517df703204f956b35617
waja <waja>
2009-05-13 15:29:31 +0000

    暫時更改失去光明的靈魂石碎片為可刪除.
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@286 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- db/Fix/Fix_etcitem.sql
+++ db/Fix/Fix_etcitem.sql
@@ -15,7 +15,10 @@ Update etcitem Set weight = '7' Where item_id = '49114';
 Update etcitem Set weight = '7' Where item_id = '49115';
 Update etcitem Set weight = '7' Where item_id = '49116';
 
-/* 2009???? waja 修正的 指定傳送卷軸(歌唱之島) 項目 */
+/* 修正 指定傳送卷軸(歌唱之島) 項目 */
 Update etcitem Set locx = '32778' Where item_id = '40082';
 Update etcitem Set locy = '32779' Where item_id = '40082';
 Update etcitem Set mapid = '68' Where item_id = '40082';
+
+/* 20090512 暫時更改失去光輝的靈魂碎片 41422 為可以刪除(任務道具不同職業打到很麻煩) */
+Update etcitem Set cant_delete = '0' Where item_id = '41422';
