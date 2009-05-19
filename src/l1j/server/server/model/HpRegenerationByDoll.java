5218f3de4585102f6a243ed959370ebcd1af6890
Tsai Castiello <lifetime520@gmail.com>
2009-05-19 15:11:52 +0000

    格式規範修正 多餘變數剔除
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@327 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- src/l1j/server/server/model/HpRegenerationByDoll.java
+++ src/l1j/server/server/model/HpRegenerationByDoll.java
@@ -31,17 +31,17 @@ public class HpRegenerationByDoll extends TimerTask {
 	}
 
 	public void regenHp() {
-        int mpr = 40;
-        if (_pc.get_food() < 3 || isOverWeight(_pc)) {
-            mpr = 0;
-        }
-        int newHp = _pc.getCurrentHp() + mpr;
-        if (newHp < 0) {
-            newHp = 0;
+		if (_pc.get_food() < 40 || isOverWeight(_pc)) { // 40 == 飽食度 17%
+			int newHp = _pc.getCurrentHp();
+		} else {
+			int newHp = _pc.getCurrentHp() + 40;
+		}
+		if (newHp < 0) {
+			newHp = 0;
 		}
 //waja add 娃娃回血效果 希爾黛絲特效
-        _pc.sendPackets(new S_SkillSound(_pc.getId(), 6321));
-        _pc.broadcastPacket(new S_SkillSound(_pc.getId(), 6321));
+		_pc.sendPackets(new S_SkillSound(_pc.getId(), 6321));
+		_pc.broadcastPacket(new S_SkillSound(_pc.getId(), 6321));
 //end
 		_pc.setCurrentHp(newHp);
 
@@ -49,7 +49,7 @@ public class HpRegenerationByDoll extends TimerTask {
 
 	private boolean isOverWeight(L1PcInstance pc) {
 		if (pc.hasSkillEffect(L1SkillId.EXOTIC_VITALIZE)
-                || pc.hasSkillEffect(L1SkillId.ADDITIONAL_FIRE)) {
+				|| pc.hasSkillEffect(L1SkillId.ADDITIONAL_FIRE)) {
 			return false;
 		}
 		return (14 < pc.getInventory().getWeight240()) ? true : false;
