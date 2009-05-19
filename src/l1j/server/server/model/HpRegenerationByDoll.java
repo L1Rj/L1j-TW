ba6f6907877dd8eda759015c50b9c3453082dfff
waja <waja>
2009-05-19 07:13:31 +0000

    希爾黛絲特效與負重回血修正
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@324 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- src/l1j/server/server/model/HpRegenerationByDoll.java
+++ src/l1j/server/server/model/HpRegenerationByDoll.java
@@ -31,16 +31,27 @@ public class HpRegenerationByDoll extends TimerTask {
 	}
 
 	public void regenHp() {
-		int newHp = _pc.getCurrentHp() + 40;
-		if (newHp < 0) {
-			newHp = 0;
+        int mpr = 40;
+        if (_pc.get_food() < 3 || isOverWeight(_pc)) {
+            mpr = 0;
+        }
+        int newHp = _pc.getCurrentHp() + mpr;
+        if (newHp < 0) {
+            newHp = 0;
 		}
-		// add 娃娃回血效果 waja 註: 效果變成幻術士的恐慌?
-		// _pc.sendPackets(new S_SkillSound(_pc.getId(), 6506));
-		// _pc.broadcastPacket(new S_SkillSound(_pc.getId(), 6506));
-		// end
+//waja add 娃娃回血效果 希爾黛絲特效
+        _pc.sendPackets(new S_SkillSound(_pc.getId(), 6321));
+        _pc.broadcastPacket(new S_SkillSound(_pc.getId(), 6321));
+//end
 		_pc.setCurrentHp(newHp);
 
 	}
 
+	private boolean isOverWeight(L1PcInstance pc) {
+		if (pc.hasSkillEffect(L1SkillId.EXOTIC_VITALIZE)
+                || pc.hasSkillEffect(L1SkillId.ADDITIONAL_FIRE)) {
+			return false;
+		}
+		return (14 < pc.getInventory().getWeight240()) ? true : false;
+	}
 	}
