c44e5d5034e06a66016501cd84283caba5060985
waja <waja>
2009-05-19 06:56:49 +0000

    一點點中文註解
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@323 96aee97a-5c11-ed42-8518-d7f33e96f5f9

---
+++ db/Update/Update_mobskill_tw.sql
@@ -0,0 +1,12 @@
+/* 20090517 怪物技能中文說明*/
+Update mobskill Set mobname = '暴走兔(冰錐)' Where mobid = '45049';
+Update mobskill Set mobname = '歐吉(範圍)' Where mobid = '45278';
+Update mobskill Set mobname = '黑暗棲林者(遠距離攻擊)' Where mobid = '45347';
+Update mobskill Set mobname = '恐怖的地獄犬(火燄)' Where mobid = '45471';
+Update mobskill Set mobname = '大王烏賊(迴轉漩渦)' Where mobid = '45734';
+Update mobskill Set mobname = '阿魯巴(加速術)' Where mobid = '45373';
+Update mobskill Set mobname = '阿魯巴(加速術)' Where mobid = '45505';
+Update mobskill Set mobname = '小安加斯(火焰噴吐)' Where mobid = '45746';
+Update mobskill Set mobname = '食腐獸(遠距離特殊攻擊)' Where mobid = '46065' AND actNo = 0;
+Update mobskill Set mobname = '地獄束縛犬(火焰噴吐)' Where mobid = '45512';
+Update mobskill Set mobname = '血騎士(迴旋斬)' Where mobid = '45527';
