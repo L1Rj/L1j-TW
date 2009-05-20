f97e9fd4ed49e02a959a7e1a093ffe4b9981209d
Tsai Castiello <lifetime520@gmail.com>
2009-05-20 20:10:38 +0000

    Fix/Fix_etcitem_tw.sql 嘗試做語法 精簡
    Insert/Iinsert_etcitem.sql -> Insert/insert_etcitem.sql
    Insert/Insert_Dorplist.sql -> Insert/insert_Dorplist.sql
    檔案名稱變更 (接獲投訴 大寫I 仗人多 欺負小寫i)
    
    Update/Update_droplist_tw.sql
    刪除放錯地方 (搞笑問題)
    
    
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@348 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- db/Fix/Fix_etcitem_tw.sql
+++ db/Fix/Fix_etcitem_tw.sql
@@ -16,9 +16,7 @@ Update etcitem Set weight = '7000' Where item_id = '49115';
 Update etcitem Set weight = '7000' Where item_id = '49116';
 
 /* 修正 指定傳送卷軸(歌唱之島) 項目 */
-Update etcitem Set locx = '32778' Where item_id = '40082';
-Update etcitem Set locy = '32779' Where item_id = '40082';
-Update etcitem Set mapid = '68' Where item_id = '40082';
+Update etcitem Set locx = '32778', locy = '32779', mapid = '68' Where item_id = '40082';
 
 /* 20090512 暫時更改失去光輝的靈魂碎片 41422 為可以刪除(任務道具不同職業打到很麻煩) */
 Update etcitem Set cant_delete = '0' Where item_id = '41422';
