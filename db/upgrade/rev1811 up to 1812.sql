f9ce9c4ea28950bc1fc28aad43cd5fd485e48dc0
waja <waja>
2009-05-22 06:55:07 +0000

    增加修正原有角色道具bless屬性
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@359 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- db/upgrade/rev1811 up to 1812.sql
+++ db/upgrade/rev1811 up to 1812.sql
@@ -16,3 +16,20 @@ alter table clan_warehouse add `bless` int(11) default NULL;
 INSERT INTO `etcitem` VALUES
 (41426, '封印スクロール', '$3258', 'scroll', 'choice', 'paper', 630, 1687, 4283, 1396, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1),
 (41427, '封印解除スクロール', '$3268', 'scroll', 'choice', 'paper', 630, 1686, 4282, 1395, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1);
+
+/* 20090522 rev1811 升級 1812 原有角色身上與倉庫道具祝福詛咒屬性修正 無舊資料伺服器可不用執行 */
+Update character_items Set bless = '1' Where bless = 'Null';
+Update character_items Set bless = '0' Where item_id > 100000 ;
+Update character_items Set bless = '2' Where item_id > 200000 ;
+
+Update clan_warehouse Set bless = '1' Where bless = 'Null';
+Update clan_warehouse Set bless = '0' Where item_id > 100000 ;
+Update clan_warehouse Set bless = '2' Where item_id > 200000 ;
+
+Update character_warehouse Set bless = '1' Where bless = 'Null';
+Update character_warehouse Set bless = '0' Where item_id > 100000 ;
+Update character_warehouse Set bless = '2' Where item_id > 200000 ;
+
+Update character_elf_warehouse Set bless = '1' Where bless = 'Null';
+Update character_elf_warehouse Set bless = '0' Where item_id > 100000 ;
+Update character_elf_warehouse Set bless = '2' Where item_id > 200000 ;
