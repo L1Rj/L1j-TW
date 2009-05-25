e7d624a671b45572819b3a7fe4fdb3ef09f877ce
chung <chung>
2009-05-25 05:56:51 +0000

    修正防具名稱及能力/刪除奇岩地監4F的日版BOSS/增加套裝效果
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@391 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- db/Fix/Fix_armor_tw.sql
+++ db/Fix/Fix_armor_tw.sql
@@ -1,2 +1,11 @@
-/* 20090516 神官斗篷名稱修正 */
+﻿/* 20090516 神官斗篷名稱修正 */
 Update armor Set name_id = '神官斗篷' Where item_id = '20067';
+
+/* 20090525 水晶臂甲能力修正 */
+Update armor Set hit_modifier = '1' Where item_id = '21104';
+Update armor Set dmg_modifier = '1' Where item_id = '21104';
+
+/* 20090525 冰之女皇的耳環LV8分類 */
+Update armor Set name = '冰之女皇的耳環LV.8(力量)' Where item_id = '21089';
+Update armor Set name = '冰之女皇的耳環LV.8(敏捷)' Where item_id = '21090';
+Update armor Set name = '冰之女皇的耳環LV.8(精神)' Where item_id = '21091';
