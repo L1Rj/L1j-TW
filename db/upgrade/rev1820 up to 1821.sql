8312a0b1f5e20afb2cb43fc0f8a0af06c317bf4b
waja <waja>
2009-05-25 11:46:25 +0000

    l1j 1821
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@395 96aee97a-5c11-ed42-8518-d7f33e96f5f9

---
+++ db/upgrade/rev1820 up to 1821.sql
@@ -0,0 +1,4 @@
+/* 20090525 rev1821 etcitem 道具變動 */
+Update etcitem Set item_type = 'treasure_box' Where item_id = 40696 ;
+Update etcitem Set 	invgfx = '957' Where item_id = 40696 ;
+Update etcitem Set 	invgfx = '2054' Where item_id = 40697 ;
