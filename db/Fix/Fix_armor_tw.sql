b5b62df30ce17efc70a645a34a29f0580b002ba8
Tsai Castiello <lifetime520@gmail.com>
2009-05-28 07:45:26 +0000

    Fix/Fix_armor_tw.sql 將翻譯部分 拿到Update_armor_te.sql
    Update/Update_armor_tw.sql
    和服 部分改為全職(否則像是龍騎GM角色 無法穿著)
    
    
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@434 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- db/Fix/Fix_armor_tw.sql
+++ db/Fix/Fix_armor_tw.sql
@@ -1,35 +1,7 @@
-/* 20090516 神官斗篷名稱修正 */
-Update armor Set name_id = '神官斗篷' Where item_id = '20067';
-
 /* 20090525 水晶臂甲能力修正 */
 Update armor Set hit_modifier = '1' Where item_id = '21104';
 Update armor Set dmg_modifier = '1' Where item_id = '21104';
 
-/* 20090525 name_id名稱修正 */
-Update armor Set name_id = '冰之女皇的耳環 Lv.0' Where item_id = '21081';
-Update armor Set name_id = '冰之女皇的耳環 Lv.1' Where item_id = '21082';
-Update armor Set name_id = '冰之女皇的耳環 Lv.2' Where item_id = '21083';
-Update armor Set name_id = '冰之女皇的耳環 Lv.3' Where item_id = '21084';
-Update armor Set name_id = '冰之女皇的耳環 Lv.4' Where item_id = '21085';
-Update armor Set name_id = '冰之女皇的耳環 Lv.5' Where item_id = '21086';
-Update armor Set name_id = '冰之女皇的耳環 Lv.6' Where item_id = '21087';
-Update armor Set name_id = '冰之女皇的耳環 Lv.7' Where item_id = '21088';
-Update armor Set name_id = '冰之女皇的耳環 Lv.8' Where item_id = '21089';
-Update armor Set name_id = '冰之女皇的耳環 Lv.8' Where item_id = '21090';
-Update armor Set name_id = '冰之女皇的耳環 Lv.8' Where item_id = '21091';
-Update armor Set name_id = '親睦耳環 (Type.1)' Where item_id = '21074';
-Update armor Set name_id = '親睦耳環 (Type.1-1)' Where item_id = '21075';
-Update armor Set name_id = '親睦耳環 (Type.1-2)' Where item_id = '21076';
-Update armor Set name_id = '親睦耳環 (Type.1-3)' Where item_id = '21077';
-Update armor Set name_id = '親睦耳環 (Type.2-1)' Where item_id = '21078';
-Update armor Set name_id = '親睦耳環 (Type.2-2)' Where item_id = '21079';
-Update armor Set name_id = '親睦耳環 (Type.2-3)' Where item_id = '21080';
-Update armor Set name_id = '思克巴耳飾' Where item_id = '21000';
-Update armor Set name_id = '雪人耳飾' Where item_id = '21001';
-Update armor Set name_id = '高崙冰石人耳飾' Where item_id = '21002';
-Update armor Set name_id = '男幽靈披風' Where item_id = '21061';
-Update armor Set name_id = '女幽靈披風' Where item_id = '21062';
-Update armor Set name_id = '男性泳裝：泳褲' Where item_id = '21063';
-Update armor Set name_id = '男性泳裝：沙灘褲' Where item_id = '21064';
-Update armor Set name_id = '女性泳裝：連身泳裝' Where item_id = '21065';
-Update armor Set name_id = '女性泳裝：比基尼' Where item_id = '21066';
+/* 和服(男性) 全職業可用 */
+Update armor Set use_dragonknight = '1', use_illusionist = '1' Where item_id = '20347'; /*和服(男性)*/
+Update armor Set use_dragonknight = '1', use_illusionist = '1' Where item_id = '20348'; /*和服(女性)*/
