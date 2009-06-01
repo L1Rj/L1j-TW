6a593ac49a67f5ee7f350060811acc5517e0cb87
chung <chung>
2009-06-01 05:57:06 +0000

    修正怪物技能順序/ 更改修正錯誤位置/
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@455 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- db/Fix/Fix_mobskill_tw.sql
+++ db/Fix/Fix_mobskill_tw.sql
@@ -67,15 +67,31 @@ Insert Into `mobskill` Values
  (45046, 0, '小獵犬', 2, 50, 0, 0, '-5', 0, 0, 0, 0, 0, 0, 16, 0, 0, 0, 0, 0, 0),
  (45047, 0, '聖伯納犬', 2, 50, 0, 0, '-5', 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0);
  
- /* 20090527 拿掉巨蟻女皇多出的技能(依照天透)*/
- delete from mobskill where mobid = '45614' AND SkillId = '10057' ;
- delete from mobskill where mobid = '45614' AND SkillId = '10071' ;
- delete from mobskill where mobid = '45614' AND SkillId = '44' ;
- delete from mobskill where mobid = '45614' AND SkillId = '10019' ;
+/* 20090527 拿掉巨蟻女皇多出的技能(依照天透)*/
+delete from mobskill where mobid = '45614' AND SkillId = '10057' ;
+delete from mobskill where mobid = '45614' AND SkillId = '10071' ;
+delete from mobskill where mobid = '45614' AND SkillId = '44' ;
+delete from mobskill where mobid = '45614' AND SkillId = '10019' ;
  
- /*20090529移除天透沒有的怪物技能*/
-  delete from mobskill where mobid = '45416' AND SkillId = '47' ;
-  delete from mobskill where mobid = '45418' AND SkillId = '47' ;
-  delete from mobskill where mobid = '45806' AND SkillId = '6' ;
-  delete from mobskill where mobid = '45874' AND SkillId = '10080' ;
- 
+/* 20090527 修正移除技能後的順序*/
+Update mobskill Set actNo = '0' Where mobid = '45614' AND actNo = '3';
+Update mobskill Set actNo = '1' Where mobid = '45614' AND actNo = '5';
+
+/*20090528 修正混沌技能順序*/
+Update mobskill Set actNo = '1' Where mobid = '45625' AND actNo = '2';
+Update mobskill Set actNo = '2' Where mobid = '45625' AND actNo = '3';
+Update mobskill Set actNo = '3' Where mobid = '45625' AND actNo = '4';
+Update mobskill Set actNo = '4' Where mobid = '45625' AND actNo = '5';
+Update mobskill Set actNo = '5' Where mobid = '45625' AND actNo = '6';
+Update mobskill Set actNo = '6' Where mobid = '45625' AND actNo = '7';
+
+/*20090529移除天透沒有的怪物技能*/
+delete from mobskill where mobid = '45416' AND SkillId = '47' ;
+delete from mobskill where mobid = '45418' AND SkillId = '47' ;
+delete from mobskill where mobid = '45806' AND SkillId = '6' ;
+delete from mobskill where mobid = '45874' AND SkillId = '10080' ;
+  
+/*20090601修正移除後的技能順序*/
+Update mobskill Set actNo = '1' Where mobid = '45416' AND actNo = '2';
+Update mobskill Set actNo = '0' Where mobid = '45418' AND actNo = '1';
+Update mobskill Set actNo = '1' Where mobid = '45418' AND actNo = '2';
--- db/Update/Update_mobskill_tw.sql
+++ db/Update/Update_mobskill_tw.sql
@@ -20,18 +20,6 @@ Update mobskill Set mobname = '混沌(召喚混沌的司祭A)' Where mobid = '45
 Update mobskill Set mobname = '混沌(召喚混沌的司祭b)' Where mobid = '45625' AND actNo = '5';
 Update mobskill Set mobname = '混沌(召喚混沌的司祭c)' Where mobid = '45625' AND actNo = '6';
 
-/* 20090527 修正移除技能後的順序*/
-Update mobskill Set actNo = '0' Where mobid = '45614' AND actNo = '3';
-Update mobskill Set actNo = '1' Where mobid = '45614' AND actNo = '5';
-
-/*20090528 修正混沌技能順序*/
-Update mobskill Set actNo = '1' Where mobid = '45625' AND actNo = '2';
-Update mobskill Set actNo = '2' Where mobid = '45625' AND actNo = '3';
-Update mobskill Set actNo = '3' Where mobid = '45625' AND actNo = '4';
-Update mobskill Set actNo = '4' Where mobid = '45625' AND actNo = '5';
-Update mobskill Set actNo = '5' Where mobid = '45625' AND actNo = '6';
-Update mobskill Set actNo = '6' Where mobid = '45625' AND actNo = '7';
-
 /*20090601 怪物技能中文化*/
 Update mobskill Set mobname = '暴走兔(冰錐)' Where mobid =45049 AND actNo = 0;
 Update mobskill Set mobname = '南瓜怪(強力攻撃)' Where mobid =45167 AND actNo = 0;
