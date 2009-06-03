ffb6b48c32ca07ff510681db5dcec4f0eada0d02
waja <waja>
2009-06-03 03:58:18 +0000

    中文翻譯
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@466 96aee97a-5c11-ed42-8518-d7f33e96f5f9

---
+++ db/Update/Update_town_tw.sql
@@ -0,0 +1,11 @@
+/* 村莊名稱 */
+Update town Set name = '說話之島' Where town_id = '1';
+Update town Set name = '銀騎士村莊' Where town_id = '2';
+Update town Set name = '古魯丁村莊' Where town_id = '3';
+Update town Set name = '燃柳村莊' Where town_id = '4';
+Update town Set name = '風木村莊' Where town_id = '5';
+Update town Set name = '肯特村莊' Where town_id = '6';
+Update town Set name = '奇岩村莊' Where town_id = '7';
+Update town Set name = '海音村莊' Where town_id = '8';
+Update town Set name = '威頓村莊' Where town_id = '9';
+Update town Set name = '歐瑞村莊' Where town_id = '10';
