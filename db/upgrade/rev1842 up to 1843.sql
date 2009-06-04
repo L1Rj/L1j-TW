d705389cf864c31886ab93914056625ddc61aa9a
waja <waja>
2009-06-04 13:57:15 +0000

    l1j 1843
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@474 96aee97a-5c11-ed42-8518-d7f33e96f5f9

---
+++ db/upgrade/rev1842 up to 1843.sql
@@ -0,0 +1,11 @@
+/* 20090604 rev1842 NPC 變更資料 */
+Update npc Set IsTU = '1' Where npcid = '46091';
+Update npc Set IsTU = '1' Where npcid = '46092';
+Update npc Set IsTU = '1' Where npcid = '46093';
+Update npc Set IsTU = '1' Where npcid = '46094';
+Update npc Set IsTU = '1' Where npcid = '46095';
+Update npc Set IsTU = '1' Where npcid = '46096';
+Update npc Set IsTU = '1' Where npcid = '46097';
+Update npc Set IsTU = '1' Where npcid = '46098';
+Update npc Set IsTU = '1' Where npcid = '46099';
+Update npc Set IsTU = '1' Where npcid = '46106';
