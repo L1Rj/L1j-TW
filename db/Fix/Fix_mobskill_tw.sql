21c39acd59c61a878ab5bcd38505a2eccdf9a918
chung <chung>
2009-06-06 13:04:01 +0000

    拿掉帥氣但是不穩的怪物skill/
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@489 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- db/Fix/Fix_mobskill_tw.sql
+++ db/Fix/Fix_mobskill_tw.sql
@@ -1,17 +1,12 @@
-/* 20090517 怪物技能補充 */
-
-/*20090606 暫時移除不完全的怪物技能*/
 /* 20090523 台版混沌無相消術 移除 */
-
 delete from mobskill where mobid = '45625' AND SkillId = '44' ;
-/*20090526 新增寵物技能*/
 
-/*暫時移除*/
 /* 20090527 拿掉巨蟻女皇多出的技能(依照天透)*/
 delete from mobskill where mobid = '45614' AND SkillId = '10057' ;
 delete from mobskill where mobid = '45614' AND SkillId = '10071' ;
 delete from mobskill where mobid = '45614' AND SkillId = '44' ;
 delete from mobskill where mobid = '45614' AND SkillId = '10019' ;
+
 /* 20090527 修正移除技能後的順序*/
 Update mobskill Set actNo = '0' Where mobid = '45614' AND actNo = '3';
 Update mobskill Set actNo = '1' Where mobid = '45614' AND actNo = '5';
@@ -29,6 +24,7 @@ delete from mobskill where mobid = '45416' AND SkillId = '47' ;
 delete from mobskill where mobid = '45418' AND SkillId = '47' ;
 delete from mobskill where mobid = '45806' AND SkillId = '6' ;
 delete from mobskill where mobid = '45874' AND SkillId = '10080' ; 
+
 /*20090601修正移除後的技能順序*/
 Update mobskill Set actNo = '1' Where mobid = '45416' AND actNo = '2';
 Update mobskill Set actNo = '0' Where mobid = '45418' AND actNo = '1';
