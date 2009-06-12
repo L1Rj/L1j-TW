1aaebcc6cbeff08ce32c736ea62c37b062b1d3aa
waja <waja>
2009-06-12 11:03:18 +0000

    修改幾種招喚怪物HP低於70-80才招喚怪物,以免一直招到全畫面都是怪.
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@536 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- db/Fix/Fix_mobskill_tw.sql
+++ db/Fix/Fix_mobskill_tw.sql
@@ -35,3 +35,32 @@ Update mobskill Set actNo = '4' Where mobid = '45625' AND actNo = '5';
 Update mobskill Set actNo = '5' Where mobid = '45625' AND actNo = '6';
 Update mobskill Set actNo = '6' Where mobid = '45625' AND actNo = '7';
 */
+
+/* 20090612 修正惡魔HP低於80%時才招喚怪物 */
+Update mobskill Set TriHp = '80' Where mobid = '45649' AND SummonId = '45174';
+Update mobskill Set TriHp = '80' Where mobid = '45649' AND SummonId = '45341';
+Update mobskill Set TriHp = '80' Where mobid = '45649' AND SummonId = '45365';
+Update mobskill Set TriHp = '80' Where mobid = '45649' AND SummonId = '45420';
+Update mobskill Set TriHp = '80' Where mobid = '45649' AND SummonId = '45373';
+Update mobskill Set TriHp = '80' Where mobid = '45649' AND SummonId = '45270';
+Update mobskill Set TriHp = '80' Where mobid = '45649' AND SummonId = '45269';
+Update mobskill Set TriHp = '80' Where mobid = '45649' AND SummonId = '45286';
+
+/* 20090612 修正死亡騎士HP低於70%時才招喚怪物 */
+Update mobskill Set TriHp = '70' Where mobid = '45601' AND SummonId = '45161';
+
+/* 20090612 修正吸血鬼HP低於70%時才招喚怪物 */
+Update mobskill Set TriHp = '70' Where mobid = '45499' AND SummonId = '45290';
+
+/* 20090612 修正魔獸軍王巴蘭卡HP低於80%時才招喚怪物 */
+Update mobskill Set TriHp = '80' Where mobid = '45844' AND SummonId = '45836';
+
+/* 20090612 修正墮落HP低於80%時才招喚怪物 */
+Update mobskill Set TriHp = '80' Where mobid = '45685' AND SummonId = '45570';
+Update mobskill Set TriHp = '80' Where mobid = '45685' AND SummonId = '45571';
+Update mobskill Set TriHp = '80' Where mobid = '45685' AND SummonId = '45582';
+Update mobskill Set TriHp = '80' Where mobid = '45685' AND SummonId = '45587';
+Update mobskill Set TriHp = '80' Where mobid = '45685' AND SummonId = '45605';
+
+/* 20090612 修正騎士范德HP低於70%時才招喚怪物 */
+Update mobskill Set TriHp = '70' Where mobid = '45618' AND SummonId = '45503';
