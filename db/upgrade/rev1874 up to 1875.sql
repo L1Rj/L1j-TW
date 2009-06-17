bbc10fde7beecf211798332b515f65a1d420281c
waja <waja>
2009-06-17 16:55:19 +0000

    l1j 1876
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@571 96aee97a-5c11-ed42-8518-d7f33e96f5f9

---
+++ db/upgrade/rev1874 up to 1875.sql
@@ -0,0 +1,6 @@
+/* 20090618 rev1875  修改 船舶之墓殭屍 資料 */
+Update mobskill Set TriRnd = '50' Where mobid  = '45720';
+Update npc Set ranged = '2' Where npcid  = '45720';
+Update skills Set castgfx = '2510' Where skill_id = '10072';
+Update skills Set damage_value = '10' Where skill_id = '10073';
+Update skills Set castgfx = '5415' Where skill_id = '10073';
