be0a8abad125ff36baa1764622ccae46e3fde0a1
waja <waja>
2009-06-17 12:05:01 +0000

    l1j 1871
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@567 96aee97a-5c11-ed42-8518-d7f33e96f5f9

---
+++ db/upgrade/rev1870 up to 1871.sql
@@ -0,0 +1,12 @@
+/* 20090617 rev1871  修改部份NPC資料 */
+Update npc Set sub_magic_speed  = '1400' Where npcid  = '45955';
+Update npc Set sub_magic_speed  = '1600' Where npcid  = '45956';
+Update npc Set sub_magic_speed  = '1520' Where npcid  = '45957';
+Update npc Set sub_magic_speed  = '1560' Where npcid  = '45958';
+Update npc Set sub_magic_speed  = '1680' Where npcid  = '45959';
+Update npc Set atk_magic_speed  = '960' Where npcid  = '46060';
+Update npc Set sub_magic_speed  = '960' Where npcid  = '46060';
+Update npc Set atk_magic_speed  = '1000' Where npcid  = '46061';
+Update npc Set sub_magic_speed  = '1000' Where npcid  = '46061';
+Update npc Set sub_magic_speed  = '1120' Where npcid  = '46065';
+Update npc Set sub_magic_speed  = '1360' Where npcid  = '46067';
