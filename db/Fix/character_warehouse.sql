d6ed946683e75405de26dc4f14c0a69cfd61e50d
chung <chung>
2009-06-22 05:08:04 +0000

    新增欄位(屬性強化部分) 不然無法順利遊戲
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@597 96aee97a-5c11-ed42-8518-d7f33e96f5f9

---
+++ db/Fix/character_warehouse.sql
@@ -0,0 +1,11 @@
+/* character_warehouse 欄位新增 */
+
+alter table character_warehouse add `firemr` int(11) default '0' ;
+alter table character_warehouse add `watermr` int(11) default '0' ;
+alter table character_warehouse add `earthmr` int(11) default '0' ;
+alter table character_warehouse add `windmr` int(11) default '0' ;
+alter table character_warehouse add `addsp` int(11) default '0' ;
+alter table character_warehouse add `addhp` int(11) default '0' ;
+alter table character_warehouse add `addmp` int(11) default '0' ;
+alter table character_warehouse add `hpr` int(11) default '0' ;
+alter table character_warehouse add `mpr` int(11) default '0' ;
