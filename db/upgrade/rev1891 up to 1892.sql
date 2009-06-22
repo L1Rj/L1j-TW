21affacb2277f53d5b96ac1c503e225f92b52d7b
waja <waja>
2009-06-22 12:03:50 +0000

    l1j 1892 & 修正昨天改錯的屬性卷軸nameid
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@599 96aee97a-5c11-ed42-8518-d7f33e96f5f9

---
+++ db/upgrade/rev1891 up to 1892.sql
@@ -0,0 +1,10 @@
+/* 2009062 rev1892 修正四屬性卷軸資料 */
+Update etcitem Set grdgfx = '6938' Where item_id = '41429';
+Update etcitem Set grdgfx = '6935' Where item_id = '41430';
+Update etcitem Set grdgfx = '6933' Where item_id = '41431';
+Update etcitem Set grdgfx = '6936' Where item_id = '41432';
+
+delete from etcitem where item_id = '49144' ;
+delete from etcitem where item_id = '49145' ;
+delete from etcitem where item_id = '49146' ;
+delete from etcitem where item_id = '49147' ;
