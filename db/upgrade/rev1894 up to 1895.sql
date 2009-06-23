7cb519715bc97c98a9f065ff52af6f95867fd238
waja <waja>
2009-06-23 13:54:26 +0000

    手誤修正
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@609 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- db/upgrade/rev1894 up to 1895.sql
+++ db/upgrade/rev1894 up to 1895.sql
@@ -1,5 +1,5 @@
 /* 20090623 rev1895 修正GM商店資料 */
-Update shop Set item_id = '41429' Where npc_id And item_id = '49144';
-Update shop Set item_id = '41430' Where npc_id And item_id = '49145';
-Update shop Set item_id = '41431' Where npc_id And item_id = '49146';
-Update shop Set item_id = '41432' Where npc_id And item_id = '49147';
+Update shop Set item_id = '41429' Where npc_id = '81028' And item_id = '49144';
+Update shop Set item_id = '41430' Where npc_id = '81028' And item_id = '49145';
+Update shop Set item_id = '41431' Where npc_id = '81028' And item_id = '49146';
+Update shop Set item_id = '41432' Where npc_id = '81028' And item_id = '49147';
