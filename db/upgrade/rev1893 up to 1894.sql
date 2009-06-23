4ba146364907352643e8f1e91c48b87011d97fda
waja <waja>
2009-06-23 12:51:11 +0000

    l1j 1894
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@607 96aee97a-5c11-ed42-8518-d7f33e96f5f9

---
+++ db/upgrade/rev1893 up to 1894.sql
@@ -0,0 +1,9 @@
+/* 20090623 rev1894 修正四屬性卷軸資料 */
+Update etcitem Set invgfx = '3030' Where item_id = '41429';
+Update etcitem Set invgfx = '3028' Where item_id = '41430';
+Update etcitem Set invgfx = '3029' Where item_id = '41431';
+Update etcitem Set invgfx = '3032' Where item_id = '41432';
+Update etcitem Set grdgfx = '6936' Where item_id = '41429';
+Update etcitem Set grdgfx = '6933' Where item_id = '41430';
+Update etcitem Set grdgfx = '6935' Where item_id = '41431';
+Update etcitem Set grdgfx = '6938' Where item_id = '41432';
