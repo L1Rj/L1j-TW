666be157c92625d0e2ff1892485d34cd633a3fcf
waja <waja>
2009-06-25 11:43:27 +0000

    移除重複新增的NPC
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@626 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- db/upgrade/l1jtw 306 up to 361.sql
+++ db/upgrade/l1jtw 306 up to 361.sql
@@ -43,20 +43,3 @@ alter table clan_warehouse add `addhp` int(11) default '0' ;
 alter table clan_warehouse add `addmp` int(11) default '0' ;
 alter table clan_warehouse add `hpr` int(11) default '0' ;
 alter table clan_warehouse add `mpr` int(11) default '0' ;
-
-/* NPC 新增 夏依藍 亞丁 34022 33107 */
-INSERT INTO `npc` (`npcid`, `name`, `nameid`, `note`, `impl`, `gfxid`, `lvl`, `hp`, `mp`, `ac`, `str`, `con`, `dex`, `wis`, `intel`, `mr`, `exp`, `lawful`, `size`, `weakAttr`, `ranged`, `tamable`, `passispeed`, `atkspeed`, `atk_magic_speed`, `sub_magic_speed`, `undead`, `poison_atk`, `paralysis_atk`, `agro`, `agrososc`, `agrocoi`, `family`, `agrofamily`, `agrogfxid1`, `agrogfxid2`, `picupitem`, `digestitem`, `bravespeed`, `hprinterval`, `hpr`, `mprinterval`, `mpr`, `teleport`, `randomlevel`, `randomhp`, `randommp`, `randomac`, `randomexp`, `randomlawful`, `damage_reduction`, `hard`, `doppel`, `IsTU`, `IsErase`, `bowActId`, `karma`, `transform_id`, `transform_gfxid`, `light_size`, `amount_fixed`, `change_head`, `cant_resurrect`) VALUES
-(91001, '夏依藍', '夏依藍', '', 'L1Merchant', 2090, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, -1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 14, 0, 1, 0);
-
-/* NPC 列表新增 夏依藍 */
-INSERT INTO `spawnlist_npc` (`id`, `location`, `count`, `npc_templateid`, `locx`, `locy`, `randomx`, `randomy`, `heading`, `respawn_delay`, `mapid`, `movement_distance`) VALUES
-(1310438, '夏依藍', 1, 91001, 34022, 33107, 0, 0, 4, 0, 4, 100);
-
-/* NPC販賣新增 91001 夏依藍 亞丁 34022 33107 */
-INSERT INTO `shop` (`npc_id`, `item_id`, `order_id`, `selling_price`, `pack_count`, `purchasing_price`) VALUES
-(91001, 40074, 0, 35000, 0, 17000),
-(91001, 40087, 0, 90000, 0, 45000),
-(91001, 49148, 0, 115000, 0, 55000);
-
-/* NPCaction 夏依藍 隨便寫個賣吃的 */
-INSERT INTO `npcaction` VALUES (91001, 'sharu1', 'sharu1', 'sharu2', '');
