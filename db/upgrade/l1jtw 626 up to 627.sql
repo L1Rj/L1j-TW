5c90a897d95b28c65e193f76e3df704cc073ab96
waja <waja>
2009-06-25 15:56:17 +0000

    修正弄錯資料格式
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@631 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- db/upgrade/l1jtw 626 up to 627.sql
+++ db/upgrade/l1jtw 626 up to 627.sql
@@ -3,8 +3,8 @@
 /* characters 欄位新增 */
 alter table characters add PKEcount int(10) default '0' after PKcount ;
 alter table characters add CWstatus int(10) default '0' after PKEcount ;
-alter table characters add LastPke int(10) default '0' after LastPk ;
-alter table characters add LastCw int(10) default '0' after LastPke ;
+alter table characters add LastPke datetime default NULL after LastPk ;
+alter table characters add LastCw datetime default NULL after LastPke ;
 
 /* 精靈公主 類型改變 */
 Update npc Set impl  = 'L1Merchant' Where npcid  = '70853';
