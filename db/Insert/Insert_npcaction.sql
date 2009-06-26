85f26a92c5a433da273a6843e2510cf3ed9f2050
waja <waja>
2009-06-26 20:16:08 +0000

    修正奇岩內城傳送師 曼孟
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@644 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- db/Insert/Insert_npcaction.sql
+++ db/Insert/Insert_npcaction.sql
@@ -2,3 +2,5 @@
 INSERT INTO `npcaction` VALUES ('91001', 'sciairen1', 'sciairen1', '', '');
 /* 寵物競速賽 NPC */
 INSERT INTO `npcaction` VALUES ('91002', 'dufo', 'dufo', '', '');
+/* 退休的侍從長－曼孟 傳送奇岩隱藏宮殿 */
+INSERT INTO `npcaction` VALUES ('91003', 'gr_mammon1', 'gr_mammon2', 'gr_mammon3', '');
