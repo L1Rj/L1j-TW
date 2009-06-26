1d6c8792e0db35e02840947d7975451090cacf12
waja <waja>
2009-06-26 11:45:30 +0000

    l1j 1903
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@636 96aee97a-5c11-ed42-8518-d7f33e96f5f9

---
+++ db/upgrade/rev1901 up to 1902.sql
@@ -0,0 +1,7 @@
+/* 20090626 mobgroup 變更資料 */
+Update mobgroup Set note  = 'ブラックナイト -捜索隊-(2)' Where id  = '1';
+Update mobgroup Set minion1_count  = '1' Where id  = '1';
+Update mobgroup Set note  = 'ファイアーエッグ(2)' Where id  = '9';
+Update mobgroup Set minion1_count  = '1' Where id  = '9';
+Update mobgroup Set note  = 'ブラック ナイト(2)' Where id  = '50';
+Update mobgroup Set minion1_count  = '1' Where id  = '50';
