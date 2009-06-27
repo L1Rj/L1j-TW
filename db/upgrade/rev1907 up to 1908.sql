9db312a168768f5dd18bb46afc12c35f5506e43f
waja <waja>
2009-06-27 12:06:48 +0000

    l1j 1908
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@653 96aee97a-5c11-ed42-8518-d7f33e96f5f9

---
+++ db/upgrade/rev1907 up to 1908.sql
@@ -0,0 +1,5 @@
+/* 20090626 mobskill 修正資料*/
+delete from mobskill where mobid = '45368' And actNo ='0';
+delete from mobskill where mobid = '45368' And actNo ='1';
+insert  into mobskill values
+(45368, 0, 'ビーストサマナー(物理攻撃)', 1, 50, 0, 0, -2, 0, 0, 2, 0, 0, 2, 0, 0, 30, 0, 0, 0, 0);
