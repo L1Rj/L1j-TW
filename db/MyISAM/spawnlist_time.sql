39f20881127ff73f287b1853907934dfd300308c
bao <bao>
2009-06-27 15:21:50 +0000

    古魯丁怨靈
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@657 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- db/MyISAM/spawnlist_time.sql
+++ db/MyISAM/spawnlist_time.sql
@@ -11,3 +11,8 @@ CREATE TABLE `spawnlist_time` (
 #----------------------------
 # Records for table spawnlist_time
 #----------------------------
+INSERT INTO `spawnlist_time` VALUES ('62086', '18:00:00', '04:00:00', '1');
+INSERT INTO `spawnlist_time` VALUES ('62087', '18:00:00', '04:00:00', '1');
+INSERT INTO `spawnlist_time` VALUES ('62088', '18:00:00', '04:00:00', '1');
+INSERT INTO `spawnlist_time` VALUES ('62089', '18:00:00', '04:00:00', '1');
+INSERT INTO `spawnlist_time` VALUES ('62092', '18:00:00', '04:00:00', '1');
