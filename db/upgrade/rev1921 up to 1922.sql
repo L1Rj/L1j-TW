4b355fa3bd4844c133f9e57d3c93a45a7a220c04
waja <waja>
2009-06-30 13:38:23 +0000

    遺漏補上
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@676 96aee97a-5c11-ed42-8518-d7f33e96f5f9

---
+++ db/upgrade/rev1921 up to 1922.sql
@@ -0,0 +1,7 @@
+/* 20090630 spawnlist 修正資料*/
+Update spawnlist Set count  = '25' Where id = '100002';
+Update spawnlist Set count  = '35' Where id = '100003';
+Update spawnlist Set count  = '30' Where id = '100004';
+Update spawnlist Set count  = '2' Where id = '100005';
+Update spawnlist Set count  = '1' Where id = '100006';
+Update spawnlist Set group_id  = '0' Where id = '100006';
