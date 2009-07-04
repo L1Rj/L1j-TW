6ef4d8d24e5db41069986962a9074eb46be48a35
waja <waja>
2009-07-04 15:09:23 +0000

    移除BOM
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@706 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- src/l1j/server/server/model/Instance/L1MerchantInstance.java
+++ src/l1j/server/server/model/Instance/L1MerchantInstance.java
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2, or (at your option)
@@ -2650,17 +2650,17 @@ public class L1MerchantInstance extends L1NpcInstance {
 				} else {
 					htmlid = "jp1";
 				}
-            		} else if (npcid ==  71181) {//示愛任務-奇岩-愛瑪伊
-               			 if (player.get_sex() == 0) {
-                		   	htmlid = "my2";
-               			 } else {
-                    			htmlid = "my1";
-               			}
-           		} else if (npcid ==  71182) {//示愛任務-奇岩-伊森
-               			 if (player.get_sex() == 0) {
-                   			htmlid = "sm2";
-                		} else {
-                  			htmlid = "sm1";
+			} else if (npcid ==  71181) {//示愛任務-奇岩-愛瑪伊
+				if (player.get_sex() == 0) {
+					htmlid = "my2";
+				} else {
+					htmlid = "my1";
+				}
+			} else if (npcid ==  71182) {//示愛任務-奇岩-伊森
+				if (player.get_sex() == 0) {
+					htmlid = "sm2";
+				} else {
+					htmlid = "sm1";
                 		}
 //哈蒙任務
 			} else if (npcid == 80094) {
