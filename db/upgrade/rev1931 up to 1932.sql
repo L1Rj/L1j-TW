f0d618ab39a10f4571d1fdc8a61774e32b208433
waja <waja>
2009-07-05 14:47:10 +0000

    l1j 1932
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@724 96aee97a-5c11-ed42-8518-d7f33e96f5f9

---
+++ db/upgrade/rev1931 up to 1932.sql
@@ -0,0 +1,6 @@
+/* 20090705 npc 修正速度資料*/
+Update npc Set atkspeed = '1720' Where npcid = '45326';/* 黑暗棲林者 */
+Update npc Set atk_magic_speed = '2200' Where npcid = '45326';
+Update npc Set sub_magic_speed = '2200' Where npcid = '45326';
+Update npc Set atk_magic_speed = '0' Where npcid = '45347';/* 黑暗棲林者 */
+Update npc Set sub_magic_speed = '2200' Where npcid = '45347';
