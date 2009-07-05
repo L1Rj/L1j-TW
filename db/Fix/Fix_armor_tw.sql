66b1c7dc8c368032ac3d2195ae1dc2ddbb848ce3
waja <waja>
2009-07-05 15:39:44 +0000

    修正三種耳環的設定
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@730 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- db/Fix/Fix_armor_tw.sql
+++ db/Fix/Fix_armor_tw.sql
@@ -5,3 +5,15 @@ Update armor Set dmg_modifier = '1' Where item_id = '21104';
 /* 和服(男性) 全職業可用 */
 Update armor Set use_dragonknight = '1', use_illusionist = '1' Where item_id = '20347'; /*和服(男性)*/
 Update armor Set use_dragonknight = '1', use_illusionist = '1' Where item_id = '20348'; /*和服(女性)*/
+
+/* 20090705 修正射擊手耳環 */
+Update armor Set bow_hit_modifier = '2' Where item_id = '21097';
+Update armor Set bow_dmg_modifier = '1' Where item_id = '21097';
+
+/* 20090705 修正劍鬥士耳環 */
+Update armor Set hit_modifier = '2' Where item_id = '21098';
+Update armor Set dmg_modifier = '1' Where item_id = '21098';
+
+/* 20090705 修正大法師耳環 */
+Update armor Set add_sp = '2' Where item_id = '21099';
+Update armor Set add_mpr = '1' Where item_id = '21099';
