ef5221ec97da73aeef2e7b3d1a0e80dd90ddac6d
waja <waja>
2009-07-07 09:13:38 +0000

    l1j 1940
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@745 96aee97a-5c11-ed42-8518-d7f33e96f5f9

---
+++ db/upgrade/rev1939 up to 1940.sql
@@ -0,0 +1,9 @@
+/* 20090707 skills 修正資料 */
+Update skills Set probability_value = '33' Where skill_id = '133';
+Update skills Set probability_value = '33' Where skill_id = '152';
+Update skills Set probability_value = '33' Where skill_id = '153';
+Update skills Set probability_value = '33' Where skill_id = '157';
+Update skills Set probability_value = '33' Where skill_id = '161';
+Update skills Set probability_value = '33' Where skill_id = '167';
+Update skills Set probability_value = '33' Where skill_id = '173';
+Update skills Set probability_value = '33' Where skill_id = '174';
