0870d81fe598225c1c2b7d8f80078306dd670246
waja <waja>
2009-07-08 11:01:08 +0000

    l1j 1944 (1943等編號確定再補)
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@752 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- db/upgrade/l1jtw 626 up to 627.sql
+++ db/upgrade/l1jtw 626 up to 627.sql
@@ -5,8 +5,3 @@ alter table characters add PKEcount int(10) default '0' after PKcount ;
 alter table characters add CWstatus int(10) default '0' after PKEcount ;
 alter table characters add LastPke datetime default NULL after LastPk ;
 alter table characters add LastCw datetime default NULL after LastPke ;
-
-/* 精靈公主 類型改變 */
-/* 移除 造成妖精任務無法傳送
-Update npc Set impl  = 'L1Merchant' Where npcid  = '70853';
-*/
---
+++ db/upgrade/l1jtw 750 up to 751.sql
@@ -0,0 +1,9 @@
+/* 20090708 l1j-tw l1j-tw用 與l1j更新無關 */
+/* droplist 移除龍騎士書板掉落 改用rev1944日板掉落 */
+delete from droplist where itemId = '49105' ;
+delete from droplist where itemId = '49106' ;
+delete from droplist where itemId = '49110' ;
+delete from droplist where itemId = '49111' ;
+delete from droplist where itemId = '49112' ;
+delete from droplist where itemId = '49114' ;
+delete from droplist where itemId = '49113' ;
