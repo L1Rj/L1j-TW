fd041df0c1a1c0f90e97e9de4803d073d0ccfe42
waja <waja>
2009-07-09 13:48:12 +0000

    海賊島第三層修正為無法瞬移
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@761 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- db/Fix/Fix_mapids_tw.sql
+++ db/Fix/Fix_mapids_tw.sql
@@ -8,3 +8,6 @@ Update mapids Set markable = '1' Where mapid = '1001';
 /* 20090627 修正 希培利亞 可標記&瞬移*/
 Update mapids Set teleportable = '1' Where mapid = '1000';
 Update mapids Set markable = '1' Where mapid = '1000';
+
+/* 20090709 修正 海賊島ケイブ:3階 不可瞬移*/
+Update mapids Set teleportable = '0' Where mapid = '443';
