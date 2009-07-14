c1cfab88222ab406a3d58250943aeebf18c11c3e
waja <waja>
2009-07-14 09:43:45 +0000

    l1j 1951
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@788 96aee97a-5c11-ed42-8518-d7f33e96f5f9

---
+++ db/upgrade/rev1950 up to 1951.sql
@@ -0,0 +1,118 @@
+/* 20090714 npc 修正資料 */
+Update npc Set passispeed = '480' Where npcid  = '45196';
+Update npc Set atkspeed = '1160' Where npcid  = '45196';
+Update npc Set atk_magic_speed  = '1160' Where npcid  = '45196';
+Update npc Set sub_magic_speed = '1160' Where npcid  = '45196';
+
+Update npc Set passispeed = '420' Where npcid  = '45355';
+Update npc Set atkspeed = '740' Where npcid  = '45355';
+Update npc Set atk_magic_speed  = '740' Where npcid  = '45355';
+Update npc Set sub_magic_speed = '740' Where npcid  = '45355';
+
+Update npc Set passispeed = '320' Where npcid  = '45358';
+Update npc Set atkspeed = '740' Where npcid  = '45358';
+Update npc Set atk_magic_speed  = '740' Where npcid  = '45358';
+Update npc Set sub_magic_speed = '740' Where npcid  = '45358';
+
+Update npc Set passispeed = '440' Where npcid  = '45362';
+Update npc Set atkspeed = '760' Where npcid  = '45362';
+Update npc Set atk_magic_speed  = '840' Where npcid  = '45362';
+Update npc Set sub_magic_speed = '840' Where npcid  = '45362';
+
+Update npc Set passispeed = '470' Where npcid  = '45364';
+Update npc Set atkspeed = '1440' Where npcid  = '45364';
+Update npc Set atk_magic_speed  = '1520' Where npcid  = '45364';
+Update npc Set sub_magic_speed = '1520' Where npcid  = '45364';
+
+Update npc Set passispeed = '650' Where npcid  = '45371';
+Update npc Set atkspeed = '840' Where npcid  = '45371';
+Update npc Set atk_magic_speed  = '840' Where npcid  = '45371';
+Update npc Set sub_magic_speed = '840' Where npcid  = '45371';
+
+Update npc Set passispeed = '480' Where npcid  = '45377';
+Update npc Set atkspeed = '680' Where npcid  = '45377';
+Update npc Set atk_magic_speed  = '680' Where npcid  = '45377';
+Update npc Set sub_magic_speed = '680' Where npcid  = '45377';
+
+Update npc Set passispeed = '680' Where npcid  = '45378';
+Update npc Set atkspeed = '540' Where npcid  = '45378';
+Update npc Set atk_magic_speed  = '540' Where npcid  = '45378';
+Update npc Set sub_magic_speed = '540' Where npcid  = '45378';
+
+Update npc Set passispeed = '630' Where npcid  = '45387';
+Update npc Set atkspeed = '1380' Where npcid  = '45387';
+Update npc Set atk_magic_speed  = '1380' Where npcid  = '45387';
+Update npc Set sub_magic_speed = '1380' Where npcid  = '45387';
+
+Update npc Set passispeed = '420' Where npcid  = '45389';
+Update npc Set atkspeed = '680' Where npcid  = '45389';
+Update npc Set atk_magic_speed  = '680' Where npcid  = '45389';
+Update npc Set sub_magic_speed = '680' Where npcid  = '45389';
+
+Update npc Set passispeed = '480' Where npcid  = '45390';
+Update npc Set atkspeed = '860' Where npcid  = '45390';
+Update npc Set atk_magic_speed  = '860' Where npcid  = '45390';
+Update npc Set sub_magic_speed = '860' Where npcid  = '45390';
+
+Update npc Set passispeed = '480' Where npcid  = '45392';
+Update npc Set atkspeed = '700' Where npcid  = '45392';
+Update npc Set atk_magic_speed  = '700' Where npcid  = '45392';
+Update npc Set sub_magic_speed = '700' Where npcid  = '45392';
+
+Update npc Set passispeed = '320' Where npcid  = '45401';
+Update npc Set atkspeed = '740' Where npcid  = '45401';
+Update npc Set atk_magic_speed  = '740' Where npcid  = '45401';
+Update npc Set sub_magic_speed = '740' Where npcid  = '45401';
+
+Update npc Set passispeed = '480' Where npcid  = '45445';
+Update npc Set atkspeed = '920' Where npcid  = '45445';
+Update npc Set atk_magic_speed  = '920' Where npcid  = '45445';
+Update npc Set sub_magic_speed = '920' Where npcid  = '45445';
+
+Update npc Set passispeed = '480' Where npcid  = '45449';
+Update npc Set atkspeed = '860' Where npcid  = '45449';
+Update npc Set atk_magic_speed  = '860' Where npcid  = '45449';
+Update npc Set sub_magic_speed = '860' Where npcid  = '45449';
+
+Update npc Set passispeed = '640' Where npcid  = '45452';
+Update npc Set atkspeed = '960' Where npcid  = '45452';
+Update npc Set atk_magic_speed  = '1080' Where npcid  = '45452';
+Update npc Set sub_magic_speed = '1080' Where npcid  = '45452';
+
+Update npc Set passispeed = '640' Where npcid  = '45469';
+Update npc Set atkspeed = '1180' Where npcid  = '45469';
+Update npc Set atk_magic_speed  = '1180' Where npcid  = '45469';
+Update npc Set sub_magic_speed = '1180' Where npcid  = '45469';
+
+Update npc Set passispeed = '680' Where npcid  = '45470';
+Update npc Set atkspeed = '540' Where npcid  = '45470';
+Update npc Set atk_magic_speed  = '540' Where npcid  = '45470';
+Update npc Set sub_magic_speed = '540' Where npcid  = '45470';
+
+Update npc Set passispeed = '620' Where npcid  = '45477';
+Update npc Set atkspeed = '960' Where npcid  = '45477';
+Update npc Set atk_magic_speed  = '960' Where npcid  = '45477';
+Update npc Set sub_magic_speed = '960' Where npcid  = '45477';
+
+Update npc Set passispeed = '440' Where npcid  = '45505';
+Update npc Set atkspeed = '760' Where npcid  = '45505';
+Update npc Set atk_magic_speed  = '1080' Where npcid  = '45505';
+Update npc Set sub_magic_speed = '1080' Where npcid  = '45505';
+
+Update npc Set passispeed = '480' Where npcid  = '45509';
+Update npc Set atkspeed = '640' Where npcid  = '45509';
+Update npc Set atk_magic_speed  = '640' Where npcid  = '45509';
+Update npc Set sub_magic_speed = '640' Where npcid  = '45509';
+
+Update npc Set passispeed = '620' Where npcid  = '45531';
+Update npc Set atkspeed = '860' Where npcid  = '45531';
+Update npc Set atk_magic_speed  = '1180' Where npcid  = '45531';
+Update npc Set sub_magic_speed = '1180' Where npcid  = '45531';
+
+Update npc Set passispeed = '640' Where npcid  = '45538';
+Update npc Set atkspeed = '780' Where npcid  = '45538';
+Update npc Set atk_magic_speed  = '780' Where npcid  = '45538';
+Update npc Set sub_magic_speed = '780' Where npcid  = '45538';
+
+Update npc Set passispeed = '280' Where npcid  = '45578';
+Update npc Set atkspeed = '640' Where npcid  = '45578';
