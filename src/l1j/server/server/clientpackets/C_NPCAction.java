4d8917ea58a98d6d76a21298dfae6d55dad80763
waja <waja>
2009-07-22 02:28:51 +0000

    移除昨天剪貼來的怪物打消失寵物問題(我們這邊沒這問題)
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@845 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- src/l1j/server/server/clientpackets/C_NPCAction.java
+++ src/l1j/server/server/clientpackets/C_NPCAction.java
@@ -509,7 +509,6 @@ public class C_NPCAction extends ClientBasePacket {
 					L1PetInstance pet = (L1PetInstance) petObject;
 					pet.collect();
 					pc.getPetList().remove(pet.getId());
-					pet.setDead(true);
 					pet.deleteMe();
 				}
 			}
