709b34a73783f8b269c38bac180c61e6d7a19a8a
waja <waja>
2009-07-23 12:12:46 +0000

    啟動由 512M改為1024M
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@860 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- ServerStart.bat
+++ ServerStart.bat
@@ -1,2 +1,2 @@
-@java -Xmx512m -Xincgc -cp l1jserver.jar;lib\c3p0-0.9.1.2.jar;lib\mysql-connector-java-5.1.7-bin.jar;lib\javolution-5.3.1.jar l1j.server.Server
+@java -Xmx1024m -Xincgc -cp l1jserver.jar;lib\c3p0-0.9.1.2.jar;lib\mysql-connector-java-5.1.7-bin.jar;lib\javolution-5.3.1.jar l1j.server.Server
 @pause
