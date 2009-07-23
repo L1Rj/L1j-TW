bfadf3121ec2efbdcabe92bb7ab04ab976c5469c
waja <waja>
2009-07-23 13:15:15 +0000

    l1j1956
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@863 96aee97a-5c11-ed42-8518-d7f33e96f5f9

---
+++ db/upgrade/l1j/rev1955 up to 1956.sql
@@ -0,0 +1,4 @@
+/* 20090723 mapids 修正 */
+Update mapids Set teleportable = '0' Where mapid  = '307';
+Update mapids Set teleportable = '0' Where mapid  = '308';
+Update mapids Set teleportable = '0' Where mapid  = '309';
