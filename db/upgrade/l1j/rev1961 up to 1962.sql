a0ff5af50ae4bbb485b4aee29455586e03a3cb74
waja <waja>
2009-07-25 13:01:56 +0000

    l1j 1962
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@873 96aee97a-5c11-ed42-8518-d7f33e96f5f9

---
+++ db/upgrade/l1j/rev1961 up to 1962.sql
@@ -0,0 +1,8 @@
+/* 20090725 npc 修正 */
+
+Update npc Set gfxid = '4744' Where npcid  = '45518';/* 重裝歐姆戰士 ラス2F */
+Update npc Set passispeed = '960' Where npcid  = '45518';
+Update npc Set atkspeed = '1040' Where npcid  = '45518';
+Update npc Set gfxid = '4744' Where npcid  = '45872';/* 重裝歐姆戰士 ラス3F */
+Update npc Set passispeed = '960' Where npcid  = '45872';
+Update npc Set atkspeed = '1040' Where npcid  = '45872';
