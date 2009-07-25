c87adb686b3748c713c22d03461bf430c2101fa6
waja <waja>
2009-07-25 17:33:28 +0000

    缺檔 補
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@880 96aee97a-5c11-ed42-8518-d7f33e96f5f9

---
+++ src/l1j/server/server/NetworkGroup.java
@@ -0,0 +1,42 @@
+package l1j.server.server;
+
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import l1j.server.Config;
+
+public class NetworkGroup
+{
+	private static NetworkGroup nGroup; // 連結工作群組
+	
+	public static NetworkGroup getGroup()
+	{
+		nGroup = nGroup == null ? new NetworkGroup() : nGroup;
+		return nGroup;
+	}
+	
+	// -- 常數 --
+	private static int cpSize = 10; // Thread要保存的數量
+	private static int mpSize = Config.MAX_ONLINE_USERS * 4 ; // Thread可容納的數量
+	private static int kaTime = 1; // 存活時間
+	private static final TimeUnit tUnit = TimeUnit.SECONDS; // 存活時間單位 (預設 秒)
+	
+	private final ThreadPoolExecutor TPExec; // 線程池
+	
+	private NetworkGroup()
+	{
+		TPExec = new ThreadPoolExecutor(cpSize, mpSize,
+				kaTime, tUnit, new SynchronousQueue<Runnable>());
+	}
+	
+	public void run(Runnable r)
+	{
+		TPExec.execute(r);
+	}
+	
+	public void del(Runnable r)
+	{
+		TPExec.remove(r);
+	}
+}
