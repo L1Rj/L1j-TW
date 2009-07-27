4db53d85bd532ce3c6352cb9b4cd16d2e65ce34b
waja <waja>
2009-07-27 17:09:21 +0000

    換一個啟動檔
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@899 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- ServerStart.bat
+++ ServerStart.bat
@@ -3,8 +3,10 @@ title L1J-TW Server Console
 REM -------------------------------------
 REM 基本的伺服器預設參數
 java -Xmx1024m -Xincgc -cp l1jserver.jar;lib\c3p0-0.9.1.2.jar;lib\mysql-connector-java-5.1.7-bin.jar;lib\javolution-5.3.1.jar l1j.server.Server
-REM 
+REM
+REM 據說會比較好的參數
+@java -Xmx512m -Xss1024K -XX:+UseConcMarkSweepGC -cp l1jserver.jar;lib\c3p0-0.9.1.2.jar;lib\mysql-connector-java-5.1.7-bin.jar;lib\javolution-5.3.1.jar l1j.server.Server
 REM 如果你有一台大的伺服器和許多的記憶體，可以嘗試下列範例
-REM java -server -Xmx1536m -Xms1024m -Xmn512m -XX:PermSize=256m -XX:SurvivorRatio=8 -Xnoclassgc -XX:+AggressiveOpts
+REM java -server -Xmx1536m -Xms1024m -Xmn512m -XX:PermSize=256m -XX:SurvivorRatio=8 -Xnoclassgc -XX:+AggressiveOpts -cp l1jserver.jar;lib\c3p0-0.9.1.2.jar;lib\mysql-connector-java-5.1.7-bin.jar;lib\javolution-5.3.1.jar l1j.server.Server
 REM -------------------------------------
 pause
