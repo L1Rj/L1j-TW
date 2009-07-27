d3aac4c83a49d2eea6969e1d936cf3dc895c4467
TaMAki <lancerevo.gtsport@gmail.com>
2009-07-27 15:39:31 +0000

    更新中文化
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@897 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- ServerStart.bat
+++ ServerStart.bat
@@ -1,2 +1,10 @@
-@java -Xmx1024m -Xincgc -cp l1jserver.jar;lib\c3p0-0.9.1.2.jar;lib\mysql-connector-java-5.1.7-bin.jar;lib\javolution-5.3.1.jar l1j.server.Server
-@pause
+@echo off
+title L1J-TW Server Console
+REM -------------------------------------
+REM 基本的伺服器預設參數
+java -Xmx1024m -Xincgc -cp l1jserver.jar;lib\c3p0-0.9.1.2.jar;lib\mysql-connector-java-5.1.7-bin.jar;lib\javolution-5.3.1.jar l1j.server.Server
+REM 
+REM 如果你有一台大的伺服器和許多的記憶體，可以嘗試下列範例
+REM java -server -Xmx1536m -Xms1024m -Xmn512m -XX:PermSize=256m -XX:SurvivorRatio=8 -Xnoclassgc -XX:+AggressiveOpts
+REM -------------------------------------
+pause
