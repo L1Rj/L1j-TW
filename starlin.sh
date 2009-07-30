a97efd8d42f4d5abf695dad9fc7b0d9f16b4494e
TaMAki <lancerevo.gtsport@gmail.com>
2009-07-30 11:40:55 +0000

    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@916 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- starlin.sh
+++ starlin.sh
@@ -8,7 +8,7 @@ export PATH="${PATH}":~/usr/lib/jdk1.6.0_13/bin
 
 while :; do
 
-/usr/lib/jdk1.6.0_13/bin/java -Xms1024m -Xmx1024m -cp lib/c3p0-0.9.1.2.jar:lib/mysql-connector-java-5.1.7-bin.jar:lib/javolution.jar:l1jserver.jar l1j.server.Server &
+/usr/lib/jdk1.6.0_13/bin/java -Xms1024m -Xmx1024m -cp lib/c3p0-0.9.1.2.jar:lib/mysql-connector-java-5.1.8-bin.jar:lib/javolution-5.3.1.jar:l1jserver.jar l1j.server.Server &
 
     [ $? -ne 2 ] && break
 
