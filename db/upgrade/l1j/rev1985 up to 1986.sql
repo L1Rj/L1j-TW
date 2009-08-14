89e4ab2a530f6d922514a84f04f1af7fc7e2cb31
waja <waja>
2009-08-14 09:14:58 +0000

    l1j 1935 手誤修正 l1j 1984 l1j 1985 l1j 1986 l1j 1987 l1j 1988
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@1003 96aee97a-5c11-ed42-8518-d7f33e96f5f9

---
+++ db/upgrade/l1j/rev1985 up to 1986.sql
@@ -0,0 +1,11 @@
+/* 20090813 spawnlist_npc 修正資料 */
+Update spawnlist_npc Set locx = '33589' ,locy = '33194' ,movement_distance ='15' Where id = '89050';
+Update spawnlist_npc Set locy = '33194' ,movement_distance ='15' Where id = '89051';
+Update spawnlist_npc Set locx = '33657' ,locy = '33221' ,movement_distance ='15' Where id = '89052';
+Update spawnlist_npc Set locx = '33657' Where id = '89053';
+Update spawnlist_npc Set locx = '33618' ,locy = '33321' ,movement_distance ='15' Where id = '89054';
+Update spawnlist_npc Set locx = '33618' ,movement_distance ='15' Where id = '89055';
+Update spawnlist_npc Set locx = '33644' ,locy = '33429' ,movement_distance ='15' Where id = '89056';
+Update spawnlist_npc Set locx = '33653' ,locy = '33427' ,movement_distance ='15' Where id = '89057';
+Update spawnlist_npc Set locx = '33607' ,locy = '33449' ,heading = '5' ,movement_distance ='15' Where id = '89058';
+Update spawnlist_npc Set locx = '33602' ,locy = '33445' ,heading = '5' ,movement_distance ='15' Where id = '89059';
