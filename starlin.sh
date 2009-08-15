dfcbe97cf62df34293859864d0e82eecfde31afe
TaMAki <lancerevo.gtsport@gmail.com>
2009-08-15 11:37:17 +0000

    Linux啟動檔加入DeadLockDetector
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@1012 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- starlin.sh
+++ starlin.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-export PATH="${PATH}":~/usr/lib/jdk1.6.0_13/bin
-
 # exit codes of GameServer:
 #  0 normal shutdown
 #  2 reboot attempt
 
-while :; do
+while :; do 
+  [ -f log/java0.log.0 ] && mv log/java0.log.0 "log/`date +%Y-%m-%d_%H-%M-%S`_java.log"
+  [ -f log/stdout.log ] && mv log/stdout.log "log/`date +%Y-%m-%d_%H-%M-%S`_stdout.log" 
+java -Xms1024m -Xmx1024m -cp l1jserver.jar:lib\l1jtw_commons.jar:lib\c3p0-0.9.1.2.jar:lib/mysql-connector-java-5.1.8-bin.jar:lib\commons-dbcp-1.3-SNAPSHOT-r796150-jdbc4.jar:lib\commons-pool-1.5.2.jar:lib\log4j-1.2.15.jar:lib\commons-io-1.4.jar:lib/javolution-5.3.1.jar l1j.server.Server > log/stdout.log 2>&1
+ [ $? -ne 2 ] && break 
+#       /etc/init.d/mysql restart 
+         sleep 10 
+done 
 
-/usr/lib/jdk1.6.0_13/bin/java -Xms1024m -Xmx1024m -cp lib\l1jtw_commons.jar:lib\c3p0-0.9.1.2.jar:lib\commons-dbcp-1.3-SNAPSHOT-r796150-jdbc4.jar:lib\commons-pool-1.5.2.jar:lib\log4j-1.2.15.jar:lib\commons-io-1.4.jar:lib/mysql-connector-java-5.1.8-bin.jar:lib/javolution-5.3.1.jar:l1jserver.jar l1j.server.Server &
-
-    [ $? -ne 2 ] && break
-
-    sleep 10
-done
