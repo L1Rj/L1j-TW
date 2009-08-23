ae711f00f950289d0ef05dc91e7a45351be45a25
Tsai Castiello <lifetime520@gmail.com>
2009-08-23 16:41:40 +0000

    註解更新
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@1047 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- src/l1j/server/server/utils/RandomArrayList.java
+++ src/l1j/server/server/utils/RandomArrayList.java
@@ -3,10 +3,12 @@ package l1j.server.server.utils;
 import java.util.Random;
 
 public class RandomArrayList {
-	private static Random _random = new Random();			 /** 亂數產生物件 */
-	/** 先宣告空間來做 隨機數值的緩存 */
-	private static int listint = 0;							 /** 泛用型隨機矩陣，所使用的指標 */
-	private static double[] ArrayDouble = new double[32767]; /** 新型泛用型，適用Int的正數範圍 */
+	/** 亂數產生物件 */
+	private static Random _random = new Random();
+	/** 泛用型隨機矩陣，所使用的指標 */
+	private static int listint = 0;
+	/** 新型泛用型，適用Int的正數範圍 */
+	private static double[] ArrayDouble = new double[32767];
 
 	public static void setArrayList() {
 		for (listint = 0; listint < 32767; listint++) {
@@ -39,8 +41,10 @@ public class RandomArrayList {
 	/* 
 	 * getInc(int 數值, int 輸出偏移值) 隨機值的僞靜態，速度是nextInt(int 數值) 的數倍
 	 * 根據呼叫的數值傳回 靜態表內加工後的數值,並採共同指標來決定傳回的依據.
+	 * EX:getInc(92988, 10) => (0~92987) + 10 => 10~92997
 	 * 
-	 * @param rang - Int類型, increase - 修正輸出結果的範圍
+	 * @param rang - Int類型
+	 * @param increase - 修正輸出結果的範圍
 	 * @return 0 ~ (數值-1) + 輸出偏移值
 	 */
 	public static int getInc(int rang, int increase) {
