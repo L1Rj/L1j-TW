78659d1c13d6f84b667c5badaf2bfcb857bea806
waja <waja>
2009-08-25 12:12:45 +0000

    l1j 2012 2013 2014
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@1051 96aee97a-5c11-ed42-8518-d7f33e96f5f9

---
+++ db/upgrade/l1j/rev2011 up to 2012.sql
@@ -0,0 +1,5 @@
+/* 20090825 spawnlist 修改資料 */
+
+Update spawnlist Set count = '0' Where id = '8200001';
+Update spawnlist Set count = '0' Where id = '8200003';
+Update spawnlist Set count = '0' Where id = '8200004';
---
+++ db/upgrade/l1j/rev2013 up to 2014.sql
@@ -0,0 +1,25 @@
+/* 20090825 spr_action 修改新增資料 */
+
+Update spr_action Set framecount = '275' Where spr_id = '123' And act_id = '0';
+Update spr_action Set framecount = '275' Where spr_id = '129' And act_id = '0';
+Update spr_action Set framecount = '148' Where spr_id = '149' And act_id = '3';
+Update spr_action Set framecount = '128' Where spr_id = '244' And act_id = '0';
+Update spr_action Set framecount = '232' Where spr_id = '1016' And act_id = '3';
+Update spr_action Set framecount = '236' Where spr_id = '1018' And act_id = '3';
+Update spr_action Set framecount = '168' Where spr_id = '1755' And act_id = '3';
+Update spr_action Set framecount = '148' Where spr_id = '2510' And act_id = '3';
+Update spr_action Set framecount = '100' Where spr_id = '3756' And act_id = '0';
+Update spr_action Set framecount = '100' Where spr_id = '3743' And act_id = '0';
+Update spr_action Set framecount = '279' Where spr_id = '4728' And act_id = '0';
+Update spr_action Set framecount = '12' Where spr_id = '6284' And act_id = '0';
+Update spr_action Set framecount = '148' Where spr_id = '7138' And act_id = '3';
+Update spr_action Set framecount = '148' Where spr_id = '6686' And act_id = '3';
+Update spr_action Set framecount = '148' Where spr_id = '6687' And act_id = '3';
+Update spr_action Set framecount = '132' Where spr_id = '6694' And act_id = '28';
+Update spr_action Set framecount = '132' Where spr_id = '6692' And act_id = '28';
+Update spr_action Set framecount = '148' Where spr_id = '6689' And act_id = '3';
+Update spr_action Set framecount = '148' Where spr_id = '6688' And act_id = '3';
+Update spr_action Set framecount = '148' Where spr_id = '6687' And act_id = '3';
+
+INSERT INTO `spr_action` VALUES
+(7183, 0, 0, 24);
