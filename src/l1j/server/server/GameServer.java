34bc3f90e53cf57692d11c7f35a9b67e5ee20caa
waja <waja>
2009-09-22 09:27:43 +0000

    Kiusbt提供 新版 lin.bin 寵物道具封包修改
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@1082 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- src/l1j/server/server/GameServer.java
+++ src/l1j/server/server/GameServer.java
@@ -150,7 +150,7 @@ public class GameServer extends Thread {
 		}
 		
 		System.out.println("=================================================");
-		System.out.println("===== Lineage 3.0C ======= L1j2021-tw1081 版 ====");
+		System.out.println("===== Lineage 3.0C ======= L1j2021-tw1082 版 ====");
 		System.out.println("=================================================");
 
 		int maxOnlineUsers = Config.MAX_ONLINE_USERS;
