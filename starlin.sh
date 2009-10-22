fba345b712bd3474d8597f7c4cc4289050397883
waja <waja>
2009-10-22 17:32:25 +0000

    修正linux執行shell
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@1131 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- starlin.sh
+++ starlin.sh
@@ -8,7 +8,7 @@ export PATH="${PATH}":~/usr/lib/jdk1.6.0_13/bin
 
 while :; do
 
-/usr/lib/jdk1.6.0_13/bin/java -Xms1024m -Xmx1024m -cp lib\c3p0-0.9.1.2.jar:lib\sqlitejdbc-v056.jar:lib/mysql-connector-java-5.1.10-bin.jar:lib/javolution-5.3.1.jar:l1jserver.jar l1j.server.Server &
+/usr/lib/jdk1.6.0_13/bin/java -Xms1024m -Xmx1024m -cp lib/sqlitejdbc-v056.jar:lib/c3p0-0.9.1.2.jar:lib/mysql-connector-java-5.1.10-bin.jar:lib/javolution-5.3.1.jar:l1jserver.jar l1j.server.Server &
 
     [ $? -ne 2 ] && break
 
