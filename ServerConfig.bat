8636e83cb885b3ee8fef8673efa4dd17c0b3cf32
TaMAki <lancerevo.gtsport@gmail.com>
2009-12-23 01:28:45 +0000

    更新 Javolution
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@1197 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- ServerConfig.bat
+++ ServerConfig.bat
@@ -1,4 +1,3 @@
 @echo off
-title L1J-Naruto Server Config
-java -cp l1jserver.jar;lib\javolution-5.4.0.jar net.l1j.tool.ConfigUserInterface
+start javaw -cp bin;lib\javolution-5.4.3.jar net.l1j.tool.ConfigManager
 cls
