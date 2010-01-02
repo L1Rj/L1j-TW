5cede5246bc274347fd989a5d0d2a2ba222c6487
Tsai Castiello <lifetime520@gmail.com>
2010-01-02 07:05:10 +0000

    格式調整
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@1221 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- src/net/l1j/server/GameServer.java
+++ src/net/l1j/server/GameServer.java
@@ -274,19 +274,19 @@ public class GameServer extends Thread {
 	/**
 	 * オンライン中のプレイヤー全てに對してkick、キャラクター情報の保存をする。
 	 */
-    public void disconnectAllCharacters() {
-        Collection<L1PcInstance> players = L1World.getInstance()
-                .getAllPlayers();
-        for (L1PcInstance pc : players) {
-            pc.getNetConnection().setActiveChar(null);
-            pc.getNetConnection().kick();
-        }
-        // 全员Kickした后に保存处理をする
-        for (L1PcInstance pc : players) {
-            ClientThread.quitGame(pc);
-            L1World.getInstance().removeObject(pc);
-        }
-    }
+	public void disconnectAllCharacters() {
+		Collection<L1PcInstance> players = L1World.getInstance()
+				.getAllPlayers();
+		for (L1PcInstance pc : players) {
+			pc.getNetConnection().setActiveChar(null);
+			pc.getNetConnection().kick();
+		}
+		// 全员Kickした后に保存处理をする
+		for (L1PcInstance pc : players) {
+			ClientThread.quitGame(pc);
+			L1World.getInstance().removeObject(pc);
+		}
+	}
 
 	private class ServerShutdownThread extends Thread {
 		private final int _secondsCount;
