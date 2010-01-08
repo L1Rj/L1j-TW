52bea23e9fb8631f6a4f1aab89f566fb228df6ff
Tsai Castiello <lifetime520@gmail.com>
2010-01-08 07:22:46 +0000

    修正筆誤 (提供者: ccxiaonan 99NETS)
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@1232 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- src/net/l1j/server/clientpackets/C_ItemUSe.java
+++ src/net/l1j/server/clientpackets/C_ItemUSe.java
@@ -1305,7 +1305,7 @@ public class C_ItemUSe extends ClientBasePacket {
 									ringlevel = 20448;
 								} else if (ringId == 41199) {
 									ringlevel = 20449;
-								} else if (ringId == 411200) {
+								} else if (ringId == 41200) {
 									ringlevel = 20450;
 								}
 								pc
