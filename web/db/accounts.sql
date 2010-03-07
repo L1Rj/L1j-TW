b35ed87aff16cdf55892f28b0cd5c903b131ffb7
waja <waja>
2010-03-07 15:35:26 +0000

    tul33al 提供 網頁資料庫 accounts.sql 缺少欄位 Character slot  已補上.
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@1289 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- web/db/accounts.sql
+++ web/db/accounts.sql
@@ -12,19 +12,20 @@ SET FOREIGN_KEY_CHECKS=0;
 -- Table structure for accounts
 -- ----------------------------
 CREATE TABLE `accounts` (
-  `login` varchar(50) NOT NULL default '',
-  `password` varchar(50) default NULL,
-  `lastactive` datetime default NULL,
-  `access_level` int(11) default NULL,
-  `ip` varchar(20) NOT NULL default '',
-  `host` varchar(255) NOT NULL default '',
-  `banned` int(11) unsigned NOT NULL default '0',
+  `login` varchar(50) NOT NULL DEFAULT '',
+  `password` varchar(100) DEFAULT NULL,
+  `lastactive` datetime DEFAULT NULL,
+  `access_level` int(11) DEFAULT NULL,
+  `ip` varchar(20) NOT NULL DEFAULT '',
+  `host` varchar(255) NOT NULL DEFAULT '',
+  `banned` int(11) unsigned NOT NULL DEFAULT '0',
+  `character_slot` int(2) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY  (`login`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='MyISAM free: 3072 kB';
 
 -- ----------------------------
 -- Records 
 -- ----------------------------
-INSERT INTO `accounts` VALUES ('admin', '43e724dd398a9fcf6c7a20a48ec4c8254a1f7646f9034fc5331ecc9eed236ed2', '0000-00-00 00:00:00', '200', '127.0.0.1', '127.0.0.1', '0');
-INSERT INTO `accounts` VALUES ('free', 'nopassword', '0000-00-00 00:00:00', '0', '127.0.0.1', '127.0.0.1', '0');
-INSERT INTO `accounts` VALUES ('system', 'nopassword', '0000-00-00 00:00:00', '0', '127.0.0.1', '127.0.0.1', '0');
+INSERT INTO `accounts` VALUES ('admin', '43e724dd398a9fcf6c7a20a48ec4c8254a1f7646f9034fc5331ecc9eed236ed2', '0000-00-00 00:00:00', '200', '127.0.0.1', '127.0.0.1', '0', '0');
+INSERT INTO `accounts` VALUES ('free', 'nopassword', '0000-00-00 00:00:00', '0', '127.0.0.1', '127.0.0.1', '0', '0');
+INSERT INTO `accounts` VALUES ('system', 'nopassword', '0000-00-00 00:00:00', '0', '127.0.0.1', '127.0.0.1', '0', '0');
