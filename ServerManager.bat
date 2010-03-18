681e3a59bb7f048648f62828ff4ab325b9abc159
TaMAki <lancerevo.gtsport@gmail.com>
2010-03-18 05:46:04 +0000

    整理
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@1304 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- ServerManager.bat
+++ ServerManager.bat
@@ -2,12 +2,12 @@
 title L1J-TW Server Console
 REM -------------------------------------
 REM 基本伺服器預設參數
-java -Xmx1024m -Xincgc -cp l1jserver.jar;lib\nimrodlf-1.1a.jar;lib\sqlitejdbc-v056.jar;lib\c3p0-0.9.1.2.jar;lib\mysql-connector-java-5.1.12-bin.jar;lib\javolution-5.4.3.jar net.l1j.Server -gui
+java -Xmx1024m -Xincgc -cp l1jserver.jar;lib\* net.l1j.Server -gui
 REM
 REM 據說會比較好的參數
-REM java -Xmx512m -Xss1024K -XX:+UseConcMarkSweepGC -cp l1jserver.jar;lib\nimrodlf-1.1a.jar;lib\sqlitejdbc-v056.jar;lib\c3p0-0.9.1.2.jar;lib\mysql-connector-java-5.1.12-bin.jar;lib\javolution-5.4.3.jar net.l1j.Server -gui
+REM java -Xmx512m -Xss1024K -XX:+UseConcMarkSweepGC -cp l1jserver.jar;lib\* net.l1j.Server -gui
 REM
 REM 如果你是大台的伺服器和許多的記憶體，可以嘗試下列範例 (jdk\jre\bin目錄下可找到client與server目錄,需將server複製到jre的bin目錄下方可執行)
-REM java -server -Xmx1536m -Xms1024m -Xmn512m -XX:PermSize=256m -XX:SurvivorRatio=8 -Xnoclassgc -XX:+AggressiveOpts -cp l1jserver.jar;lib\nimrodlf-1.1a.jar;lib\sqlitejdbc-v056.jar;lib\c3p0-0.9.1.2.jar;lib\mysql-connector-java-5.1.12-bin.jar;lib\javolution-5.4.3.jar net.l1j.Server -gui
+REM java -server -Xmx1536m -Xms1024m -Xmn512m -XX:PermSize=256m -XX:SurvivorRatio=8 -Xnoclassgc -XX:+AggressiveOpts -cp l1jserver.jar;lib\* net.l1j.Server -gui
 REM -------------------------------------
 pause
