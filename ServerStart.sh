0ed8c10abbbd4fe0ed91bf9823faf21e2e84fc05
TaMAki <lancerevo.gtsport@gmail.com>
2010-03-18 05:26:17 +0000

    1.mysql-connector-java-5.1.12-bin.jar 2.spr_action update to 3.2 (author: basang)
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@1303 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- ServerStart.sh
+++ ServerStart.sh
@@ -7,7 +7,7 @@
  export PATH="${PATH}":~/usr/lib/jdk1.6.0_13/bin
  
  while :; do 
- /usr/lib/jdk1.6.0_13/bin/java -Xms1024m -Xmx1024m -cp l1jserver.jar:lib/nimrodlf-1.1a.jar:lib/sqlitejdbc-v056.jar:lib/c3p0-0.9.1.2.jar:lib/mysql-connector-java-5.1.11-bin.jar:lib/javolution-5.4.3.jar net.l1j.Server & 
+ /usr/lib/jdk1.6.0_13/bin/java -Xms1024m -Xmx1024m -cp l1jserver.jar:lib/nimrodlf-1.1a.jar:lib/sqlitejdbc-v056.jar:lib/c3p0-0.9.1.2.jar:lib/mysql-connector-java-5.1.12-bin.jar:lib/javolution-5.4.3.jar net.l1j.Server & 
   [ $? -ne 2 ] && break 
 #       /etc/init.d/mysql restart 
          sleep 10 
