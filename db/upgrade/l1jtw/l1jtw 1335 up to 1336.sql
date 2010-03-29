34b683b124ff38cdc4f55b302a19b6d7ed36b87e
waja <waja>
2010-03-29 10:17:28 +0000

    1.spawnlist_npc 位置修正 2.GMroom 新增威頓
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@1338 96aee97a-5c11-ed42-8518-d7f33e96f5f9

---
+++ db/upgrade/l1jtw/l1jtw 1335 up to 1336.sql
@@ -0,0 +1,3 @@
+/* 20100329 l1jtw NPC 91050 91051 action */
+INSERT INTO `npcaction` VALUES ('91050', 'veil1', 'veil1', '', '');
+INSERT INTO `npcaction` VALUES ('91051', 'dragonsmap', 'dragonsmap', '', '');
---
+++ db/upgrade/l1jtw/l1jtw 1337 up to 1338.sql
@@ -0,0 +1,16 @@
+/* 20100329 l1jtw spawnlist_npc 放置地圖修正 多魯嘉貝爾 屠龍佈告欄 班酷 強韌的海斯 細心的修樂 頑強的歐浩 燦爛的艾咪 米米 魔法師梅琳 */
+
+Update spawnlist_npc Set mapid = '4' Where npc_templateid = '91050';
+Update spawnlist_npc Set mapid = '4' Where npc_templateid = '91052';
+Update spawnlist_npc Set mapid = '4' Where npc_templateid = '91053';
+Update spawnlist_npc Set mapid = '4' Where npc_templateid = '91054';
+Update spawnlist_npc Set mapid = '4' Where npc_templateid = '91055';
+Update spawnlist_npc Set mapid = '4' Where npc_templateid = '91056';
+Update spawnlist_npc Set mapid = '4' Where npc_templateid = '91057';
+Update spawnlist_npc Set mapid = '4' Where npc_templateid = '91058';
+Update spawnlist_npc Set mapid = '4' Where npc_templateid = '91062';
+Update spawnlist_npc Set locx = '33711' Where npc_templateid = '91062';
+Update spawnlist_npc Set locy = '32824' Where npc_templateid = '91062';
+
+/* 多魯嘉貝爾  nameid 修正 */
+Update npc Set nameid = '$7744' Where npcid = '91050';
