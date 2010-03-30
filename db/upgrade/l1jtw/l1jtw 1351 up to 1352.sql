4200cb02afcb5f9bb10c60b00ceba15c8799c443
waja <waja>
2010-03-30 16:58:08 +0000

    BAO提供 NPC gfxid 修正
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@1352 96aee97a-5c11-ed42-8518-d7f33e96f5f9

---
+++ db/upgrade/l1jtw/l1jtw 1351 up to 1352.sql
@@ -0,0 +1,8 @@
+/* 20100331 l1jtw BAO提供  NPC gfxid 多魯嘉貝爾7618  海斯2255  修樂1621 歐浩782 艾咪6239 卡瑞1049 */
+
+Update npc Set gfxid = '7618' Where npcid = '91050';
+Update npc Set gfxid = '2255' Where npcid = '91057';
+Update npc Set gfxid = '1621' Where npcid = '91058';
+Update npc Set gfxid = '782' Where npcid = '91059';
+Update npc Set gfxid = '6239' Where npcid = '91060';
+Update npc Set gfxid = '1049' Where npcid = '91062';
