8191f639bcb1f88d8ca85a9867e18fcc4afa8ada
waja <waja>
2010-03-30 14:56:14 +0000

    1.修正防具名稱與對應. 2.手誤修正.
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@1351 96aee97a-5c11-ed42-8518-d7f33e96f5f9

---
+++ db/upgrade/l1jtw/l1jtw 1350 up to 1351.sql
@@ -0,0 +1,101 @@
+/* 20100330 l1jtw 修正防具名稱與名稱對應資料 */
+
+Update armor Set name = '守護團戒指' Where item_id = '20286';
+Update armor Set unidentified_name_id = '$3008' Where item_id = '20286';
+Update armor Set identified_name_id = '$3008' Where item_id = '20286';
+
+Update armor Set name = '情人戒指' Where item_id = '20305';
+Update armor Set unidentified_name_id = '$1379' Where item_id = '20305';
+Update armor Set identified_name_id = '$1379' Where item_id = '20305';
+
+Update armor Set name = '韓服(男性)' Where item_id = '20347';
+Update armor Set unidentified_name_id = '$3613' Where item_id = '20347';
+Update armor Set identified_name_id = '$3613' Where item_id = '20347';
+
+Update armor Set name = '和服(女性)' Where item_id = '20348';
+Update armor Set unidentified_name_id = '$3217' Where item_id = '20348';
+Update armor Set identified_name_id = '$3217' Where item_id = '20348';
+
+Update armor Set name = '獵犬項鍊' Where item_id = '20349';
+Update armor Set unidentified_name_id = '$4448' Where item_id = '20349';
+Update armor Set identified_name_id = '$4448' Where item_id = '20349';
+
+Update armor Set name = '萬聖節南瓜盔甲' Where item_id = '21067';
+Update armor Set unidentified_name_id = '$5255' Where item_id = '21067';
+Update armor Set identified_name_id = '$5255' Where item_id = '21067';
+
+Update armor Set name = 'ID．信任戒指' Where item_id = '21510';
+Update armor Set unidentified_name_id = '$7377' Where item_id = '21510';
+Update armor Set identified_name_id = '$7377' Where item_id = '21510';
+
+Update armor Set name = 'ID．勇氣戒指' Where item_id = '21511';
+Update armor Set unidentified_name_id = '$7378' Where item_id = '21511';
+Update armor Set identified_name_id = '$7378' Where item_id = '21511';
+
+Update armor Set name = 'ID．信賴戒指' Where item_id = '21512';
+Update armor Set unidentified_name_id = '$737' Where item_id = '21512';
+Update armor Set identified_name_id = '$737' Where item_id = '21512';
+
+Update armor Set name = 'ID．友情戒指' Where item_id = '21513';
+Update armor Set unidentified_name_id = '$7380' Where item_id = '21513';
+Update armor Set identified_name_id = '$7380' Where item_id = '21513';
+
+Update armor Set name = 'ID．永恆戒指' Where item_id = '21514';
+Update armor Set unidentified_name_id = '$7381' Where item_id = '21514';
+Update armor Set identified_name_id = '$7381' Where item_id = '21514';
+
+Update armor Set name = 'ID．愛情戒指' Where item_id = '21515';
+Update armor Set unidentified_name_id = '$7376' Where item_id = '21515';
+Update armor Set identified_name_id = '$7376' Where item_id = '21515';
+
+Update armor Set name = '提卡爾庫庫爾坎面具' Where item_id = '21540';
+Update armor Set unidentified_name_id = '$6431' Where item_id = '21540';
+Update armor Set identified_name_id = '$6431' Where item_id = '21540';
+
+Update armor Set name = '提卡爾庫庫爾坎之盾' Where item_id = '21541';
+Update armor Set unidentified_name_id = '$6430' Where item_id = '21541';
+Update armor Set identified_name_id = '$6430' Where item_id = '21541';
+
+Update armor Set name = '提卡爾杰弗雷庫尖牙' Where item_id = '21542';
+Update armor Set unidentified_name_id = '$6432' Where item_id = '21542';
+Update armor Set identified_name_id = '$6432' Where item_id = '21542';
+
+Update armor Set name = '提卡爾杰弗雷庫之眼' Where item_id = '21543';
+Update armor Set unidentified_name_id = '$6433' Where item_id = '21543';
+Update armor Set identified_name_id = '$6433' Where item_id = '21543';
+
+Update armor Set name = '奧拉奇里亞的鋼盔' Where item_id = '21544';
+Update armor Set unidentified_name_id = '$6376$128' Where item_id = '21544';
+Update armor Set identified_name_id = '$6376$128' Where item_id = '21544';
+
+Update armor Set name = '堅固的奧拉奇里亞的鋼盔' Where item_id = '21544';
+Update armor Set unidentified_name_id = '$6390' Where item_id = '21544';
+Update armor Set identified_name_id = '$6390' Where item_id = '21544';
+
+Update armor Set name = '蚩尤鎧甲' Where item_id = '21551';
+Update armor Set unidentified_name_id = '$5909' Where item_id = '21551';
+Update armor Set identified_name_id = '$5909' Where item_id = '21551';
+
+Update armor Set name = '席琳的樹葉冠冕' Where item_id = '21549';
+Update armor Set unidentified_name_id = '$6375$6381' Where item_id = '21549';
+Update armor Set identified_name_id = '$6375$6381' Where item_id = '21549';
+
+Update armor Set name = '發光的席琳樹葉冠冕' Where item_id = '21550';
+Update armor Set unidentified_name_id = '$6389' Where item_id = '21550';
+Update armor Set identified_name_id = '$6389' Where item_id = '21550';
+
+Update armor Set name = '哈維變身頭巾' Where item_id = '21552';
+Update armor Set unidentified_name_id = '$5570' Where item_id = '21552';
+Update armor Set identified_name_id = '$5570' Where item_id = '21552';
+
+Update armor Set name = '黑法師變身頭巾' Where item_id = '21553';
+Update armor Set unidentified_name_id = '$5571' Where item_id = '21553';
+Update armor Set identified_name_id = '$5571' Where item_id = '21553';
+
+Update armor Set name = '聖誕老人帽' Where item_id = '21554';
+Update armor Set unidentified_name_id = '$6335' Where item_id = '21554';
+Update armor Set identified_name_id = '$6335' Where item_id = '21554';
+
+Update armor Set name = '喵小編領巾' Where item_id = '21556';
+Update armor Set unidentified_name_id = '$5447' Where item_id = '21556';
+Update armor Set identified_name_id = '$5447' Where item_id = '21556';
