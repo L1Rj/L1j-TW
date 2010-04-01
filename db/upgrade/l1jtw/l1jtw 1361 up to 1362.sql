3a288d396e4c6005c20761df3e8f770b4edac016
waja <waja>
2010-04-01 18:45:41 +0000

    手誤修正.
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@1363 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- db/upgrade/l1jtw/l1jtw 1361 up to 1362.sql
+++ db/upgrade/l1jtw/l1jtw 1361 up to 1362.sql
@@ -1,4 +1,5 @@
-/* 20100402 l1jtw 多魯嘉之袋延遲時間與次數修正 */
+/* 20100402 l1jtw 多魯嘉之袋延遲時間與次數修正 不可堆疊 */
 
-Update etcitem Set max_charge_count = '30' Where item_id = '49300';
-Update etcitem Set delay_time = '86400' Where item_id = '49300';
+Update etcitem Set max_charge_count = '30' Where item_id = '50500';
+Update etcitem Set delay_time = '86400' Where item_id = '50500';
+Update etcitem Set stackable = '' Where item_id = '50500';
