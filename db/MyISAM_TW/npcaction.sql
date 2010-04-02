9821a1ba63d01894c9a17c34fdb779c2497aa2ee
waja <waja>
2010-04-02 12:10:50 +0000

    1.新增 NPC Action 強韌的海斯 細心的修樂 頑強的歐浩 燦爛的艾咪 米米 卡瑞 帕利特 伊帕奇 隱匿的巨龍谷入口 2.增加 四龍的盔甲製作/魔眼解封印與混合(仍缺NpcAction.java中道具不符送出網頁等判斷)
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@1366 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- db/MyISAM_TW/npcaction.sql
+++ db/MyISAM_TW/npcaction.sql
@@ -1061,4 +1061,14 @@ INSERT INTO `npcaction` VALUES ('91006', 'adenflora1', 'adenflora1', 'adenflora2
 INSERT INTO `npcaction` VALUES ('91007', 'amelia', 'amelia1', '', '');
 INSERT INTO `npcaction` VALUES ('91050', 'veil1', 'veil1', '', '');
 INSERT INTO `npcaction` VALUES ('91051', 'dragonsmap', 'dragonsmap', '', '');
-INSERT INTO `npcaction` VALUES ('91065', 'merlin1', 'merlin1', 'merlin2', '');
+INSERT INTO `npcaction` VALUES ('91056', 'bankoo1', 'bankoo1', '', '');				/* 班酷 */
+INSERT INTO `npcaction` VALUES ('91057', 'fivelviin1', 'fivelviin1', '', '');		/* 強韌的海斯 */
+INSERT INTO `npcaction` VALUES ('91058', 'fivelviin2', 'fivelviin2', '', '');		/* 細心的修樂 */
+INSERT INTO `npcaction` VALUES ('91059', 'fivelviin3', 'fivelviin3', '', '');		/* 頑強的歐浩 */
+INSERT INTO `npcaction` VALUES ('91060', 'fivelviin4', 'fivelviin4', '', '');		/* 燦爛的艾咪 */
+INSERT INTO `npcaction` VALUES ('91061', 'sherme2', 'sherme2', '', '');				/* 米米 */
+INSERT INTO `npcaction` VALUES ('91062', 'grayknight1', 'grayknight1', '', '');		/* 卡瑞 */
+INSERT INTO `npcaction` VALUES ('91063', 'familyed', 'familyedl', 'pfamilyed', '');	/* 帕利特 */
+INSERT INTO `npcaction` VALUES ('91064', 'ilbakiel', 'ilbakiel', '', '');			/* 伊帕奇 */
+INSERT INTO `npcaction` VALUES ('91065', 'merlin1', 'merlin1', 'merlin2', '');		/* 魔法師 梅琳 */
+INSERT INTO `npcaction` VALUES ('91066', 'dsecret1', 'dsecret2', 'dsecret3', '');	/* 隱匿的巨龍谷入口 */
