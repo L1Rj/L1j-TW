c0fa5151463cb5a4f2850e47d03fda0fbc8fcc17
waja <waja>
2010-05-18 15:18:25 +0000

    BAO提供 魔眼使用延遲時間1hr
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@1475 96aee97a-5c11-ed42-8518-d7f33e96f5f9

---
+++ db/upgrade/l1jtw/l1jtw 1474 up to 1475.sql
@@ -0,0 +1,9 @@
+/* 20100518 l1jtw 修正魔眼延遲使用時間 */
+
+Update etcitem Set delay_time = '3600' Where item_id = '50508';
+Update etcitem Set delay_time = '3600' Where item_id = '50509';
+Update etcitem Set delay_time = '3600' Where item_id = '50510';
+Update etcitem Set delay_time = '3600' Where item_id = '50511';
+Update etcitem Set delay_time = '3600' Where item_id = '50512';
+Update etcitem Set delay_time = '3600' Where item_id = '50513';
+Update etcitem Set delay_time = '3600' Where item_id = '50514';
