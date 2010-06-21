371a3c4cd292cf4a360571a8576cae3e0f943e00
jeffwen <jeffwen>
2010-06-21 15:18:53 +0000

    修正登入時選單無法即時更新
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@1538 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- L1jWeb/ASP.net/MasterPage.master.vb
+++ L1jWeb/ASP.net/MasterPage.master.vb
@@ -4,8 +4,30 @@ Imports System.Net
 Partial Class MasterPage
     Inherits System.Web.UI.MasterPage
 
+    Dim _appfunc As AppFunction
+
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        
+        If Session("LoginUser") IsNot Nothing Then
+            Dim _loginuser As LoginUser = DirectCast(Session("LoginUser"), LoginUser)
+            GetControlPanel(_loginuser)
+        End If
     End Sub
+
+    Private Sub GetControlPanel(ByVal _loginuser As LoginUser)
+        If _loginuser.Access_Level >= 200 Then
+            _appfunc = New AppFunction
+            pnlControlPage.Visible = True
+
+            '*****設定頁面上方工具列*****
+            Dim myTR1 As New TableRow()
+            '---回首頁設定---
+            Dim myTC1 As New TableCell()
+            '依權限加入功能選單
+            _appfunc.Authority_Menu(_loginuser, myTR1)
+
+            tbFunction.Rows.Add(myTR1)
+        End If
+    End Sub
+
 End Class
 
