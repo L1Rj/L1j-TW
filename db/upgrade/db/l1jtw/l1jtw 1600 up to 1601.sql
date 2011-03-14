2c2bf78a05fe084f3245de32c8081325bd1800df
waja <waja>
2011-03-14 17:18:51 +0000

    VG提供 象牙塔武防具與泡水防具安定值變更
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@1601 96aee97a-5c11-ed42-8518-d7f33e96f5f9

---
+++ db/upgrade/db/l1jtw/l1jtw 1600 up to 1601.sql
@@ -0,0 +1,29 @@
+/* 20110315 l1jtw 象牙塔&泡水武防具安定值變更 */
+/* 象牙塔防具 */
+Update armor Set safenchant = '4' Where item_id = '20028';
+Update armor Set safenchant = '4' Where item_id = '20082';
+Update armor Set safenchant = '4' Where item_id = '20126';
+Update armor Set safenchant = '4' Where item_id = '20173';
+Update armor Set safenchant = '4' Where item_id = '20206';
+Update armor Set safenchant = '4' Where item_id = '20232';
+
+/* 泡水的防具 */
+Update armor Set safenchant = '4' Where item_id = '21051';
+Update armor Set safenchant = '4' Where item_id = '21052';
+Update armor Set safenchant = '4' Where item_id = '21053';
+Update armor Set safenchant = '4' Where item_id = '21054';
+Update armor Set safenchant = '4' Where item_id = '21055';
+Update armor Set safenchant = '4' Where item_id = '21056';
+
+/* 象牙塔武器 */
+Update weapon Set safenchant = '6' Where item_id = '7';
+Update weapon Set safenchant = '6' Where item_id = '35';
+Update weapon Set safenchant = '6' Where item_id = '48';
+Update weapon Set safenchant = '6' Where item_id = '73';
+Update weapon Set safenchant = '6' Where item_id = '105';
+Update weapon Set safenchant = '6' Where item_id = '120';
+Update weapon Set safenchant = '6' Where item_id = '147';
+Update weapon Set safenchant = '6' Where item_id = '156';
+Update weapon Set safenchant = '6' Where item_id = '174';
+Update weapon Set safenchant = '6' Where item_id = '175';
+Update weapon Set safenchant = '6' Where item_id = '224';
