61c204d5bd2bd505ae7502cb7f5dca60a728a2c7
waja <waja>
2011-03-17 12:15:35 +0000

    安塔瑞斯&法力昂 怪物資料修正
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@1632 96aee97a-5c11-ed42-8518-d7f33e96f5f9

---
+++ db/upgrade/db/l1jtw/l1jtw 1631 up to 1632.sql
@@ -0,0 +1,3 @@
+/* 20110317 l1jtw 安塔瑞斯  法力昂 資料庫修正 */
+Update npc Set lvl = '80' ,hp = '30000' ,ac = '-80' ,mp = '5000' ,lawful  = '-8000' Where npcid = '45682';
+Update npc Set lvl = '85' ,hp = '120000' ,ac = '-80' ,mp = '5000' ,lawful  = '-8000' Where npcid = '45683';
