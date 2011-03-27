2135b48ad8b89115f71f3739e916eab0a5e8931a
waja <waja>
2011-03-27 12:35:56 +0000

    Rev1667 修正 補上缺的門
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@1669 96aee97a-5c11-ed42-8518-d7f33e96f5f9

--- src/net/l1j/server/clientpackets/C_Door.java
+++ src/net/l1j/server/clientpackets/C_Door.java
@@ -73,22 +73,22 @@ public class C_Door extends ClientBasePacket {
 				CloseTimer closetimer = new CloseTimer(door);
 				closetimer.begin();
 			}
-// 安塔瑞斯地圖
-		} else if (door.getDoorId() == 6033) { // 安塔瑞斯攻略型第一門
+// 安塔瑞斯地圖門 尚未驗證動作與道具
+		} else if (door.getDoorId() == 6100) { // 安塔瑞斯攻略型第一門
 			if (door.getOpenStatus() == ActionCodes.ACTION_Open) {
             	return;
 				}
 			if (pc.getInventory().consumeItem(50528, 1)) { //喀瑪王之心
 				door.open();
 			}
-		} else if (door.getDoorId() == 6034) { // 安塔瑞斯攻略型第二門
+		} else if (door.getDoorId() == 6101) { // 安塔瑞斯攻略型第二門
 			if (door.getOpenStatus() == ActionCodes.ACTION_Open) {
 			return;
 			}
 			if (pc.getInventory().consumeItem(50529, 1)) { //喀瑪王之心
 				door.open();
 			}
-		} else if (door.getDoorId() == 6035) { // 安塔瑞斯攻略型第三門
+		} else if (door.getDoorId() == 6102) { // 安塔瑞斯攻略型第三門
 			if (door.getOpenStatus() == ActionCodes.ACTION_Open) {
            	 return;
 			}
@@ -98,7 +98,7 @@ public class C_Door extends ClientBasePacket {
 			if (pc.getInventory().consumeItem(50529, 1)) { //喀瑪王之心
           	  door.open();
 			}
-		} else if (door.getDoorId() == 6035) { // 安塔瑞斯攻略型第三門
+		} else if (door.getDoorId() == 6102) { // 安塔瑞斯攻略型第三門
 			if (door.getOpenStatus() == ActionCodes.ACTION_Open) {
 			return;
 			}
