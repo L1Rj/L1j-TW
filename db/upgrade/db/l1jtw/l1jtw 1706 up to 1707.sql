4e656b146f6428ba35e90a2b2c263beaeaad259a
waja <waja>
2011-04-10 17:12:20 +0000

    修正 指定傳送卷軸(隱藏之谷) 座標
    git-svn-id: http://www.castiello-tsai.org:8523/svn/L1JTW@1707 96aee97a-5c11-ed42-8518-d7f33e96f5f9

---
+++ db/upgrade/db/l1jtw/l1jtw 1706 up to 1707.sql
@@ -0,0 +1,4 @@
+/* 20110411 l1jtw 修正指定傳送卷軸(隱藏之谷) 座標 */
+Update etcitem Set locx  = '32684' Where item_id  = '40101';
+Update etcitem Set locy  = '32865' Where item_id  = '40101';
+Update etcitem Set mapid  = '2005' Where item_id  = '40101';
